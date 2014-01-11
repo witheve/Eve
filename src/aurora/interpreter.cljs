e01e2dc6aa80c47c505f302be11d7367ef19a4eb
Jamie Brandon <jamie@scattered-thoughts.net>
2014-01-11 04:29:58 +0000

    Scope + recursion

--- src/aurora/interpreter.cljs
+++ src/aurora/interpreter.cljs
@@ -1,24 +1,26 @@
 (ns aurora.interpreter)
 
-(defn run-node [node inputs actions]
+(defn run-node [program node inputs actions]
+  (prn node inputs)
   (case (:type node)
-    :value (run-value node inputs actions)
-    :cljs (run-cljs node inputs actions)
-    :match (run-match node inputs actions)
-    :pipe (run-pipe node inputs actions)))
+    :match (run-match program node inputs actions)
+    :ref (run-ref program node inputs actions)
+    :pipe (run-pipe program node inputs actions)))
 
-(defn run-value [node inputs actions]
-  (:value node))
+(defn run-ref [program node inputs actions]
+  (case (:kind node)
+    :value (:value node)
+    :cljs (apply (:fn node) inputs)
+    :aurora (run-node program (get program (:id node)) inputs actions)))
 
-(defn run-cljs [node inputs actions]
-  (apply (:fn node) inputs))
-
-(defn run-pipe [node inputs actions]
+(defn run-pipe [program node inputs actions]
   (let [vars #js {}]
     (doseq [[id value] (map vector (:inputs node) inputs)]
       (aset vars id value))
     (doseq [{:keys [id inputs node]} (:nodes node)]
-      (aset vars id (run-node node (map #(aget vars %) inputs) actions)))
+      (assert (not= :pipe (:type node)) "No inline pipes")
+      (aset vars id (run-node program node (map #(aget vars %) inputs) actions))
+      (prn vars))
     (aget vars (-> node :nodes last :id))))
 
 (defrecord MatchFailure [])
@@ -26,68 +28,98 @@
 (defn check [bool]
   (when-not bool (throw (MatchFailure.))))
 
-(defn run-match [node inputs actions]
+(defn run-match [program node inputs actions]
   (assert (= 1 (count inputs)))
   (let [input (first inputs)]
     (loop [branches (:branches node)]
       (if-let [[{:keys [pattern inputs node]} & branches] (seq branches)]
         (try
           (let [vars #js {}]
-            (run-match-pattern pattern input actions vars)
-            (run-node node (map #(aget vars %) inputs) actions))
+            (run-match-pattern program pattern input actions vars)
+            (run-node program node (map #(aget vars %) inputs) actions))
           (catch MatchFailure _
             (recur branches)))
         (throw (MatchFailure.))))))
 
-(defn run-match-pattern [pattern input actions vars]
+(defn run-match-pattern [program pattern input actions vars]
   (cond
    (= (:type pattern) :match/bind) (do (aset vars (:var pattern) input)
-                                    (run-match-pattern (:pattern pattern) input actions vars))
-   (:type pattern) (check (run-node pattern [input] actions))
+                                    (run-match-pattern program (:pattern pattern) input actions vars))
+   (:type pattern) (check (run-node program pattern [input] actions))
    (or (number? pattern) (string? pattern)) (check (= pattern input))
    (or (nil? pattern) (sequential? pattern)) (if-let [[first-pattern & rest-pattern] (seq pattern)]
                                              (do (check (and (sequential? input) (seq input)))
                                                (let [[first-input & rest-input] input]
-                                                 (run-match-pattern first-pattern first-input actions vars)
-                                                 (run-match-pattern rest-pattern rest-input actions vars)))
+                                                 (run-match-pattern program first-pattern first-input actions vars)
+                                                 (run-match-pattern program rest-pattern rest-input actions vars)))
                                              (check (empty? input)))
    (map? pattern) (do (check (map? input))
                    (doseq [[key val] pattern]
                      (check (contains? input key))
-                     (run-match-pattern val (get input key) actions vars)))
+                     (run-match-pattern program val (get input key) actions vars)))
    :otherwise (assert false)))
-
-(def example-a
-  {:type :pipe
-   :inputs ["a" "b" "c"]
-   :nodes [{:id "b-squared" :inputs ["b" "b"] :node {:type :cljs :fn *}}
-           {:id "four" :inputs [] :node {:type :value :value 4}}
-           {:id "four-a-c" :inputs ["four" "a" "c"] :node {:type :cljs :fn *}}
-           {:id "result" :inputs ["b-squared" "four-a-c"] :node {:type :cljs :fn -}}]})
 
 (defn bind [var pattern]
   {:type :match/bind
    :var var
    :pattern pattern})
 
+(def example-a
+  {"root" {:type :pipe
+           :inputs ["a" "b" "c"]
+           :nodes [{:id "b-squared" :inputs ["b" "b"] :node {:type :ref :kind :cljs :name *}}
+                   {:id "four" :inputs [] :node {:type :ref :kind :value :value 4}}
+                   {:id "four-a-c" :inputs ["four" "a" "c"] :node {:type :ref :kind :cljs :fn *}}
+                   {:id "result" :inputs ["b-squared" "four-a-c"] :node {:type :ref :kind :cljs :fn -}}]}})
+
 (def example-b
-  {:type :match
-   :branches [{:pattern {"a" (bind "a" {:type :cljs :fn number?}) "b" (bind "b" {:type :cljs :fn number?})}
-               :inputs ["a" "b"]
-               :node {:type :cljs :fn -}}
-              {:pattern [(bind "x" {:type :value :value true}) "foo"]
-               :inputs ["x" "y"]
-               :node {:type :cljs :fn identity}}]})
+  {"root" {:type :match
+           :branches [{:pattern {"a" (bind "a" {:type :ref :kind :cljs :fn number?}) "b" (bind "b" {:type :ref :kind :cljs :fn number?})}
+                       :inputs ["a" "b"]
+                       :node {:type :ref :kind :cljs :fn -}}
+                      {:pattern [(bind "x" {:type :ref :kind :value :value true}) "foo"]
+                       :inputs ["x" "y"]
+                       :node {:type :ref :kind :cljs :fn identity}}]}})
+
+(def example-c
+  {"root" {:type :ref :kind :aurora :id "even?"}
+   "even?" {:type :match
+            :branches [{:pattern 0
+                        :inputs []
+                        :node {:type :ref :kind :value :value true}}
+                       {:pattern (bind "x" {:type :ref :kind :value :value true})
+                        :inputs ["x"]
+                        :node {:type :ref :kind :aurora :id "even?not-0"}}]}
+   "even?not-0" {:type :pipe
+                 :inputs ["x"]
+                 :nodes [{:id "one" :inputs [] :node {:type :ref :kind :value :value 1}}
+                         {:id "x-1" :inputs ["x" "one"] :node {:type :ref :kind :cljs :fn -}}
+                         {:id "result" :inputs ["x-1"] :node {:type :ref :kind :aurora :id "odd?"}}]}
+   "odd?" {:type :match
+           :branches [{:pattern 0
+                       :inputs []
+                       :node {:type :ref :kind :value :value false}}
+                      {:pattern (bind "x" {:type :ref :kind :value :value true})
+                       :inputs ["x"]
+                       :node {:type :ref :kind :aurora :id "odd?not-0"}}]}
+   "odd?not-0" {:type :pipe
+                :inputs ["x"]
+                :nodes [{:id "one" :inputs [] :node {:type :ref :kind :value :value 1}}
+                        {:id "x-1" :inputs ["x" "one"] :node {:type :ref :kind :cljs :fn -}}
+                        {:id "result" :inputs ["x-1"] :node {:type :ref :kind :aurora :id "even?"}}]}})
 
 (defn run-example [example inputs]
   (try
-    (run-node example inputs nil)
+    (run-node example (get example "root") inputs nil)
     (catch MatchFailure e e)))
 
-(run-node example-a [1 4 2] nil)
+(run-example example-a [1 4 2])
 
 (run-example example-b [{"a" 1 "b" 2}])
 (run-example example-b [{"a" 1 "c" 2}])
 (run-example example-b [{"a" 1 "b" "foo"}])
 (run-example example-b [[1 "foo"]])
 (run-example example-b [[1 2]])
+
+(run-example example-c [0])
+(run-example example-c [1])
