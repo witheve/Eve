aeaeef406f9181f3d22b9490f13d6fdb10008ead
Jamie Brandon <jamie@scattered-thoughts.net>
2014-01-15 01:48:42 +0000

    Record stack. Catch exceptions. Change program structure to set of pipes.

--- src/aurora/interpreter.cljs
+++ src/aurora/interpreter.cljs
@@ -2,81 +2,85 @@
 
 ;; interpreter
 
-(defn run-pipe [program node inputs actions]
-  (prn node inputs)
-  (assert (= :pipe (:type node)))
-  (let [vars #js {}]
+(defn run-pipe [program node inputs stack]
+  (let [vars #js {}
+        calls #js []
+        frame #js {:id (:id node) :vars vars :calls calls}]
+    (.push stack frame) ; call
+    (assert (= :pipe (:type node)))
     (doseq [[id value] (map vector (:inputs node) inputs)]
       (aset vars id value))
     (doseq [{:keys [id inputs node]} (:nodes node)]
       (assert (not= :pipe (:type node)) "No inline pipes")
-      (aset vars id (run-node program node (map #(aget vars %) inputs) actions))
-      (prn vars))
-    (aget vars (-> node :nodes last :id))))
+      (aset vars id (run-node program node (map #(aget vars %) inputs) calls))) ; note calls, not stack
+    (let [result (aget vars (-> node :nodes last :id))]
+      (aset frame "result" result) ; return
+      result)))
 
-(defn run-node [program node inputs actions]
+(defn run-node [program node inputs stack]
   (case (:type node)
-    :data (run-data program node inputs actions)
-    :match (run-match program node inputs actions)
-    :ref (run-ref program node inputs actions)))
+    :data (run-data program node inputs stack)
+    :match (run-match program node inputs stack)
+    :ref (run-ref program node inputs stack)))
 
-(defn run-data [program node inputs actions]
+(defn run-data [program node inputs stack]
   (case (:kind node)
     :value (:value node)
     :vector (into []
                   (for [value (:value node)]
-                    (run-data program value inputs actions)))
+                    (run-data program value inputs stack)))
     :map (into {}
                (for [[key value] (:value node)]
-                 [(run-data program key inputs actions) (run-data program value inputs actions)]))))
+                 [(run-data program key inputs stack) (run-data program value inputs stack)]))))
 
-(defn run-ref [program node inputs actions]
+(defn run-ref [program node inputs stack]
   (case (:kind node)
     :cljs (apply (:fn node) inputs)
-    :pipe (run-pipe program (get program (:id node)) inputs actions)))
+    :pipe (run-pipe program (get program (:id node)) inputs stack)))
 
 (defrecord MatchFailure [])
 
 (defn check [bool]
   (when-not bool (throw (MatchFailure.))))
 
-(defn run-match [program node inputs actions]
+(defn run-match [program node inputs stack]
   (assert (= 1 (count inputs)))
   (let [input (first inputs)]
     (loop [branches (:branches node)]
       (if-let [[{:keys [pattern inputs node]} & branches] (seq branches)]
         (try
           (let [vars #js {}]
-            (run-match-pattern program pattern input actions vars)
-            (run-node program node (map #(aget vars %) inputs) actions))
+            (run-match-pattern program pattern input stack vars)
+            (run-node program node (map #(aget vars %) inputs) stack))
           (catch MatchFailure _
             (recur branches)))
         (throw (MatchFailure.))))))
 
-(defn run-match-pattern [program pattern input actions vars]
+(defn run-match-pattern [program pattern input stack vars]
   (condp = (:type pattern)
     :match/any nil
     :match/bind (do (aset vars (:var pattern) input)
-                  (run-match-pattern program (:pattern pattern) input actions vars))
+                  (run-match-pattern program (:pattern pattern) input stack vars))
     :data (case (:kind pattern)
             :value (check (= input (:value pattern)))
             :vector (let [value (:value pattern)]
                       (check (vector? input))
                       (check (= (count input) (count value)))
                       (dotimes [i (count value)]
-                        (run-match-pattern program (nth value i) (nth input i) actions vars)))
+                        (run-match-pattern program (nth value i) (nth input i) stack vars)))
             :map (let [value (:value pattern)]
                    (check (map? input))
                    (doseq [key-pattern (keys value)]
                      (let [key (:value key-pattern)]
                        (check (contains? input key))
-                       (run-match-pattern program (get value key-pattern) (get input key) actions vars)))))
-    (check (run-node program pattern [input] actions))))
+                       (run-match-pattern program (get value key-pattern) (get input key) stack vars)))))
+    (check (run-node program pattern [input] stack))))
 
 ;; ast
 
-(defn pipe [inputs & nodes]
+(defn pipe [id inputs & nodes]
   {:type :pipe
+   :id id
    :inputs inputs
    :nodes (for [[id inputs node] (partition 3 nodes)]
             {:id id :inputs inputs :node node})})
@@ -118,41 +122,43 @@
 ;; examples
 
 (def example-a
-  {"root" (pipe ["a" "b" "c"]
-                "b-squared" ["b" "b"] (cljs-ref *)
-                "four" [] (data-value 4)
-                "four-a-c" ["four" "a" "c"] (cljs-ref *)
-                "result" ["b-squared" "four-a-c"] (cljs-ref -))})
+  #{(pipe "root" ["a" "b" "c"]
+          "b-squared" ["b" "b"] (cljs-ref *)
+          "four" [] (data-value 4)
+          "four-a-c" ["four" "a" "c"] (cljs-ref *)
+          "result" ["b-squared" "four-a-c"] (cljs-ref -))})
 
 (def example-b
-  {"root" (pipe ["x"]
-                "result" ["x"] (match (data-map (data-value "a") (bind "a" (cljs-ref number?)) (data-value "b") (bind "b" (cljs-ref number?))) ["a" "b"] (cljs-ref -)
+  #{(pipe "root" ["x"]
+          "result" ["x"] (match (data-map (data-value "a") (bind "a" (cljs-ref number?)) (data-value "b") (bind "b" (cljs-ref number?))) ["a" "b"] (cljs-ref -)
                                       (data-vector (bind "x" any) (data-value "foo")) ["x"] (cljs-ref identity)))})
 
 (def example-c
-  {"root" (pipe ["x"]
-                "result" ["x"] (pipe-ref "even?"))
-   "even?" (pipe ["x"]
-                 "result" ["x"] (match (data-value 0) [] (data-value true)
-                                       (bind "x" any) ["x"] (pipe-ref "even?not-0")))
-   "even?not-0" (pipe ["x"]
-                      "one" [] (data-value 1)
-                      "x-1" ["x" "one"] (cljs-ref -)
-                      "odd?" ["x-1"] (pipe-ref "odd?")
-                      "result" ["odd?"] (cljs-ref not))
-   "odd?" (pipe ["x"]
-                "result" ["x"] (match (data-value 0) [] (data-value true)
-                                      (bind "x" any) ["x"] (pipe-ref "odd?not-0")))
-   "odd?not-0" (pipe ["x"]
-                     "one" [] (data-value 1)
-                     "x-1" ["x" "one"] (cljs-ref -)
-                     "even?" ["x-1"] (pipe-ref "even?")
-                     "result" ["even?"] (cljs-ref not))})
+  #{(pipe "root" ["x"]
+          "result" ["x"] (pipe-ref "even?"))
+    (pipe "even?" ["x"]
+          "result" ["x"] (match (data-value 0) [] (data-value true)
+                                (bind "x" any) ["x"] (pipe-ref "even?not-0")))
+    (pipe "even?not-0" ["x"]
+          "one" [] (data-value 1)
+          "x-1" ["x" "one"] (cljs-ref -)
+          "odd?" ["x-1"] (pipe-ref "odd?")
+          "result" ["odd?"] (cljs-ref not))
+    (pipe "odd?" ["x"]
+          "result" ["x"] (match (data-value 0) [] (data-value true)
+                                (bind "x" any) ["x"] (pipe-ref "odd?not-0")))
+    (pipe "odd?not-0" ["x"]
+          "one" [] (data-value 1)
+          "x-1" ["x" "one"] (cljs-ref -)
+          "even?" ["x-1"] (pipe-ref "even?")
+          "result" ["even?"] (cljs-ref not))})
 
 (defn run-example [example inputs]
-  (try
-    (run-pipe example (get example "root") inputs nil)
-    (catch MatchFailure e e)))
+  (let [stack #js []
+        program (into {} (for [pipe example] [(:id pipe) pipe]))]
+    (try
+      [(run-pipe program (get program "root") inputs stack) (aget stack 0)]
+      (catch :default e [e stack]))))
 
 (run-example example-a [1 4 2])
 
