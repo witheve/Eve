aeeeb1a11e145fc0d9e9bf5e3a8ee18a7a0df196
Jamie Brandon <jamie@scattered-thoughts.net>
2014-03-04 20:13:44 -0800

    Add new to jsth

--- src/aurora/compiler/jsth.cljs
+++ src/aurora/compiler/jsth.cljs
@@ -83,6 +83,8 @@
                        (str "(function (" (join ", " (map name->string (nth x 1))) ") {\n"
                             (indent (statement->string (nth x 2))) "\n"
                             "})"))
+   (= "new" (head x)) (do (check (= (count x) 2))
+                        (str "new " (expression->string (nth x 1))))
    (seq? x) (do (check (>= (count x) 1))
               (let [f (expression->string (nth x 0))
                     args (map expression->string (rest x))]
