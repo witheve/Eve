beb2c68597dd9312b0b5352e43db828a0d7f197e
Jamie Brandon <jamie@scattered-thoughts.net>
2014-04-29 15:30:10 -0700

    O(log(n)) seek for iterators

--- src/aurora/btree.cljs
+++ src/aurora/btree.cljs
@@ -1,53 +1,99 @@
 (ns aurora.btree
   (:require-macros [aurora.macros :refer [apush]]))
 
-(deftype Tree [min-child-nodes max-child-nodes child-node])
+(deftype Tree [min-children max-children root])
 
-(deftype Branch [parent-node parent-ix keys vals child-nodes])
+(deftype Node [parent parent-ix keys vals children])
 
-(deftype Leaf [parent-node parent-ix keys vals])
-
-(deftype Iterator [^:mutable child-node ^:mutable ix]
+(deftype Iterator [^:mutable node ^:mutable ix]
   Object
   (next [this]
-        (if (< ix (alength (.-keys child-node)))
-          (let [result #js [(aget (.-keys child-node) ix) (aget (.-vals child-node) ix)]]
-            (if-let [new-child-node (and (.-child-nodes child-node) (aget (.-child-nodes child-node) ix))]
+        (if (< ix (alength (.-keys node)))
+          (let [result #js [(aget (.-keys node) ix) (aget (.-vals node) ix)]]
+            ;; check for a child
+            (if-let [child (and (.-children node) (aget (.-children node) ix))]
               ;; jump into child
               (do
-                (set! child-node new-child-node)
+                (set! node child)
                 (set! ix 0))
               ;; move along
               (set! ix (+ ix 1)))
             result)
-          (if-let [parent-node (.-parent-node child-node)]
+          (if-let [parent (.-parent node)]
             ;; jump into parent and start again
             (do
-              (set! ix (+ (.-parent-ix child-node) 1))
-              (set! child-node parent-node)
+              (set! ix (+ (.-parent-ix node) 1))
+              (set! node parent)
               (recur))
             ;; end of tree
-            nil))))
+            nil)))
+  (seek [this key] ;; move the iterator forwards until it reaches a key greater than this one
+        ;; head across and upwards until we reach a greater key
+        (loop []
+          (if (< ix (alength (.-keys node)))
+            (if (<= key (aget (.-keys node) ix))
+              ;; done
+              nil
+              ;; move along
+              (do
+                (set! ix (+ ix 1))
+                (recur)))
+            (if-let [parent (.-parent node)]
+              ;; jump into parent and start again
+              (do
+                (set! ix (+ (.-parent-ix node) 1))
+                (set! node parent)
+                (recur))
+              ;; end of tree
+              nil)))
+        ;; head downwards and across until we reach the least greater key
+        (loop []
+          (if (< ix (alength (.-keys node)))
+            (if (<= key (aget (.-keys node) ix))
+              ;; check for a child
+              (if-let [child (and (.-children node) (> ix 0) (aget (.-children node) (- ix 1)))]
+                ;; jump in child and start again (assumes child cannot be empty)
+                (do
+                  (set! node child)
+                  (set! ix 0)
+                  (recur))
+                ;; done
+                nil)
+              ;; move along
+              (do
+                (set! ix (+ ix 1))
+                (recur)))
+            ;; end of tree
+            nil))
+        (if (< ix (alength (.-keys node)))
+          #js [(aget (.-keys node) ix) (aget (.-vals node) ix)]
+          nil)))
 
 (defn iterator [tree]
-  (Iterator. (.-child-node tree) 0))
+  (Iterator. (.-root tree) 0))
 
-(defn tree [min-child-node max-child-nodes]
-  (Tree. min-child-nodes max-child-nodes (Branch. nil nil #js [] #js [] #js[])))
+(defn tree [min-child max-children]
+  (Tree. min-children max-children (Node. nil nil #js [] #js [] #js[])))
 
 (comment
-  (let [tree (Tree. min-child-nodes max-child-nodes (Branch. nil nil #js [] #js [] #js[]))
+  (let [tree (Tree. min-children max-children (Node. nil nil #js [] #js [] #js[]))
         iterator (iterator tree)]
     (take-while identity (repeatedly #(.next iterator))))
 
-  (let [tree (Tree. min-child-nodes max-child-nodes (Branch. nil nil #js [:a :b :c] #js [0 1 2] #js[]))
+  (let [tree (Tree. min-children max-children (Node. nil nil #js [:a :b :c] #js [0 1 2] #js[]))
         iterator (iterator tree)]
     (take-while identity (repeatedly #(.next iterator))))
 
-  (let [branch (Branch. nil nil #js [:a :d :e] #js [0 3 4] #js[])
-        _ (apush (.-child-nodes branch) (Leaf. branch 0 #js [:b :c] #js [1 2]))
-        tree (Tree. min-child-nodes max-child-nodes branch)
+  (let [branch (Node. nil nil #js [:a :d :e] #js [0 3 4] #js[])
+        _ (apush (.-children branch) (Node. branch 0 #js [:b :c] #js [1 2]))
+        tree (Tree. min-children max-children branch)
         iterator (iterator tree)]
     (take-while identity (repeatedly #(.next iterator))))
+
+  (let [branch (Node. nil nil #js [:a :d :f] #js [0 3 4] #js[])
+        _ (apush (.-children branch) (Node. branch 0 #js [:b :c] #js [1 2]))
+        tree (Tree. min-children max-children branch)
+        iterator (iterator tree)]
+    [(.seek iterator :c) (.seek iterator :a) (.seek iterator :e) (.seek iterator :e) (.seek iterator :p)])
 
   )
