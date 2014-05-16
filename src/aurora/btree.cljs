97ce05a25ce833135351216240135cffbd9692bb
Jamie Brandon <jamie@scattered-thoughts.net>
2014-05-16 14:25:32 -0700

    Yeah...

--- src/aurora/btree.cljs
+++ src/aurora/btree.cljs
@@ -93,7 +93,19 @@
         false))))
 
 (defn ^boolean key-gt [as bs]
-  (== 1 (key-compare as bs)))
+  (let [as-len (alength as)
+        bs-len (alength bs)]
+    (assert (== as-len bs-len) (pr-str as bs))
+    (loop [i 0]
+      (if (< i as-len)
+        (let [a (aget as i)
+              b (aget bs i)]
+          (if (or (and (identical? (typeof a) (typeof b))
+                       (> a b))
+                  (> (typeof a) (typeof b)))
+            true
+            false))
+        false))))
 
 (defn ^boolean key-lte [as bs]
   (not (== 1 (key-compare as bs))))
