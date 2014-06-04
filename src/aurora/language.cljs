2180e80c8a9d5177d9c516e575d04438f9290739
Jamie Brandon <jamie@scattered-thoughts.net>
2014-06-03 18:38:40 -0700

    Tick

--- src/aurora/language.cljs
+++ src/aurora/language.cljs
@@ -2,40 +2,6 @@
   (:require [aurora.btree :as btree])
   (:require-macros [aurora.macros :refer [apush set!!]]))
 
-;; FLOWS
-
-(deftype Flow [solver output-kinds output-names output-fields]
-  Object
-  (run [this kn rule->dirty? kind->name->rules]
-       (.reset solver)
-       (let [facts (.keys solver)]
-         (dotimes [i (alength output-kinds)]
-           (let [kind (aget output-kinds i)
-                 name (aget output-names i)
-                 fields (aget output-fields i)]
-             (when (true? (.add-facts kn kind name fields facts))
-               (let [dirtied-rules (aget kind->name->rules kind name)]
-                 (dotimes [j (alength dirtied-rules)]
-                   (aset rule->dirty? (aget dirtied-rules i) true)))))))))
-
-(deftype Flows [rules rule->flow rule->dirty? kind->name->rules]
-  Object
-  (run [this kn]
-       ;; assume everything is dirty at the start
-       (dotimes [i (alength rules)]
-         (aset rule->dirty? (aget rules i) true))
-
-       (loop [i 0]
-         (prn :loop i)
-         (when (< i (alength rules))
-           (let [rule (aget rules i)]
-             (if (true? (aget rule->dirty? rule))
-               (do
-                 (aset rule->dirty? rule false)
-                 (.run (aget rule->flow rule) kn rule->dirty? kind->name->rules)
-                 (recur 0))
-               (recur (+ i 1))))))))
-
 ;; KNOWLEDGE
 
 (defn keymap [from-fields to-fields]
@@ -58,7 +24,15 @@
   (let [changed? false
         keymap (keymap from-fields to-fields)]
     (dotimes [i (alength from-array)]
-      (when-not (.assoc! to-index (with-keymap keymap (aget from-array i)) nil)
+      (when (false? (.assoc! to-index (with-keymap keymap (aget from-array i)) nil))
+        (set!! changed? true)))
+    changed?))
+
+(defn -del-facts [from-array from-fields to-index to-fields]
+  (let [changed? false
+        keymap (keymap from-fields to-fields)]
+    (dotimes [i (alength from-array)]
+      (when (true? (.dissoc! to-index (with-keymap keymap (aget from-array i))))
         (set!! changed? true)))
     changed?))
 
@@ -76,17 +50,89 @@
              (assert (or (= kind "know") (= kind "remember") (= kind "forget")) (pr-str kind))
              (let [changed? false
                    indexes (get-in kind->name->fields->index [kind name])]
-               (assert (seq indexes))
+               (assert (seq indexes) (pr-str kind name))
                (doseq [[other-fields other-index] indexes]
                  (when (-add-facts facts fields other-index (into-array other-fields))
                    (set!! changed? true)))
                changed?))
-  (tick [this])
-  ;; TODO Knowledge.tick using array of flows, needs dirty tracking per flow (requires map from index to flow)
-  (tock [this]))
+  (del-facts [this kind name fields facts]
+             (assert (or (= kind "know") (= kind "remember") (= kind "forget")) (pr-str kind))
+             (let [changed? false
+                   indexes (get-in kind->name->fields->index [kind name])]
+               (assert (seq indexes) (pr-str kind name))
+               (doseq [[other-fields other-index] indexes]
+                 (when (-del-facts facts fields other-index (into-array other-fields))
+                   (set!! changed? true)))
+               changed?))
+  (clear-facts [this name]
+               (doseq [[_ index] (get-in kind->name->fields->index ["know" name])]
+                 (.reset index)))
+  (update-facts [this name]
+                (let [[fields remember-index] (first (get-in kind->name->fields->index ["remember" name]))]
+                  (assert (not (nil? fields)) name)
+                  (let [fields (into-array fields)
+                        forget-index (.get-or-create-index this "forget" name fields)
+                        remember-iter (btree/iterator remember-index)
+                        forget-iter (btree/iterator forget-index)
+                        remembers #js []
+                        forgets #js []]
+                    (.foreach remember-index
+                              (fn [key]
+                                (let [forget (.seek-gte forget-iter key)]
+                                  (when (or (nil? forget) (btree/key-not= key forget))
+                                    (.push remembers key)))))
+                    (.foreach forget-index
+                              (fn [key]
+                                (let [remember (.seek-gte remember-iter key)]
+                                  (when (or (nil? remember) (btree/key-not= key remember))
+                                    (.push forgets key)))))
+                    (.add-facts this "know" name fields remembers)
+                    (.del-facts this "know" name fields forgets))))
+  (tick [this name->transient?]
+        (let [names (js/Object.keys name->transient?)]
+          (dotimes [i (alength names)]
+            (let [name (aget names i)]
+              (if (true? (aget name->transient? name))
+                (.clear-facts kn name)
+                (.update-facts kn name)))))))
 
 (defn knowledge []
   (Knowledge. {}))
+
+;; FLOWS
+
+(deftype Flow [solver output-kinds output-names output-fields]
+  Object
+  (run [this kn rule->dirty? kind->name->rules]
+       (.reset solver)
+       (let [facts (.keys solver)]
+         (dotimes [i (alength output-kinds)]
+           (let [kind (aget output-kinds i)
+                 name (aget output-names i)
+                 fields (aget output-fields i)]
+             (when (true? (.add-facts kn kind name fields facts))
+               (let [dirtied-rules (aget kind->name->rules kind name)]
+                 (dotimes [j (alength dirtied-rules)]
+                   (aset rule->dirty? (aget dirtied-rules i) true)))))))))
+
+(deftype Flows [rules rule->flow rule->dirty? kind->name->rules name->transient?]
+  Object
+  (run [this kn]
+       ;; assume everything is dirty at the start
+       (dotimes [i (alength rules)]
+         (aset rule->dirty? (aget rules i) true))
+
+       (loop [i 0]
+         (when (< i (alength rules))
+           (let [rule (aget rules i)]
+             (if (true? (aget rule->dirty? rule))
+               (do
+                 (aset rule->dirty? rule false)
+                 (.run (aget rule->flow rule) kn rule->dirty? kind->name->rules)
+                 (recur 0))
+               (recur (+ i 1)))))))
+  (tick [this kn]
+        (.tick kn name->transient?)))
 
 ;; COMPILER
 
@@ -100,6 +146,7 @@
 (defn compile [kn]
   (let [rule->flow (atom {})
         kind->name->rules (atom {})
+        name->transient? (atom {})
         clauses (.keys (.get-or-create-index kn "know" "clauses" #js ["rule-id" "when|know|remember|forget" "clause-id" "name"]))
         fields (.keys (.get-or-create-index kn "know" "clause-fields" #js ["clause-id" "constant|variable" "key" "val"]))
         rule-id->clauses (atom (into {} (for [[k vs] (group-by #(nth % 0) clauses)] [k (set (map vec vs))])))
@@ -144,9 +191,6 @@
 
         (let [var->ix (zipmap @vars (range))
               num-vars (count @vars)
-
-              ;; order clause vars
-
 
               ;; make inputs
               constraints&ixes (for [[_ clause-type clause-id name] clauses
@@ -215,15 +259,25 @@
                                    (aset output-fields (var->ix val) key))
                                  output-fields)))]
 
-          ;; ensure at least one index per output
+          ;; set up state for outputs
           (dotimes [i (alength output-kinds)]
-            (swap! kind->name->rules update-in [(aget output-kinds i) (aget output-names i)] #(or % #{}))
-            (.get-or-create-index kn (aget output-kinds i) (aget output-names i) (into-array (filter #(not (nil? %)) (aget output-fields i)))))
+            (let [kind (aget output-kinds i)
+                  name (aget output-names i)
+                  fields (aget output-fields i)
+                  transient? (identical? kind "know")]
+              ;; TODO get transient? from schema instead
+              (assert (not= (not transient?) (get @name->transient? name)) name)
+              (swap! name->transient? assoc name transient?)
+              (swap! kind->name->rules update-in [kind name] #(or % #{}))
+              (.get-or-create-index kn "know" name (into-array (filter #(not (nil? %)) (aget output-fields i))))
+              (when (false? transient?)
+                (.get-or-create-index kn "forget" name (into-array (filter #(not (nil? %)) (aget output-fields i))))
+                (.get-or-create-index kn "remember" name (into-array (filter #(not (nil? %)) (aget output-fields i)))))))
 
           (swap! rule->flow assoc rule-id (Flow. solver output-kinds output-names output-fields)))))
 
     ;; TODO stratify
-    (Flows. (clj->js (map first @rule->flow)) (clj->js @rule->flow) #js {} (clj->js @kind->name->rules))))
+    (Flows. (clj->js (map first @rule->flow)) (clj->js @rule->flow) #js {} (clj->js @kind->name->rules) (clj->js @name->transient?))))
 
 ;; TESTS
 
@@ -258,9 +312,9 @@
                                                                                              #js ["output-transitive-connected" "variable" "x" "xx"]
                                                                                              #js ["output-transitive-connected" "variable" "y" "zz"]])
 
-(.add-facts kn "know" "clauses" #js ["rule-id" "when|know|remember|forget" "clause-id" "name"] #js [#js ["function-edge" "when" "get-function-edge" "edge"]
+(.add-facts kn "know" "clauses" #js ["rule-id" "when|know|remember|forget" "clause-id" "name"] #js [#js ["function-edge" "when" "get-function-edge" "connected"]
                                                                                                     #js ["function-edge" "when" "make-str" "=function"]
-                                                                                                    #js ["function-edge" "know" "know-str" "str-edge"]])
+                                                                                                    #js ["function-edge" "remember" "know-str" "str-edge"]])
 
 (.add-facts kn "know" "clause-fields" #js ["clause-id" "constant|variable" "key" "val"] #js [#js ["get-function-edge" "variable" "x" "xx"]
                                                                                              #js ["get-function-edge" "variable" "y" "yy"]
@@ -268,13 +322,33 @@
                                                                                              #js ["make-str" "constant" "js" "\"edge \" + xx + \" \" + yy"]
                                                                                              #js ["know-str" "variable" "name" "zz"]])
 
+(.add-facts kn "know" "clauses" #js ["rule-id" "when|know|remember|forget" "clause-id" "name"] #js [#js ["-function-edge" "when" "-get-function-edge" "edge"]
+                                                                                                    #js ["-function-edge" "when" "-make-str" "=function"]
+                                                                                                    #js ["-function-edge" "forget" "-know-str" "str-edge"]])
+
+(.add-facts kn "know" "clause-fields" #js ["clause-id" "constant|variable" "key" "val"] #js [#js ["-get-function-edge" "variable" "x" "xx"]
+                                                                                             #js ["-get-function-edge" "variable" "y" "yy"]
+                                                                                             #js ["-make-str" "variable" "variable" "zz"]
+                                                                                             #js ["-make-str" "constant" "js" "\"edge \" + xx + \" \" + yy"]
+                                                                                             #js ["-know-str" "variable" "name" "zz"]])
+
 (def flows (compile kn))
 
-(set! (.-rules flows) #js ["single-edge" "transitive-edge" "function-edge"])
+(set! (.-rules flows) #js ["single-edge" "transitive-edge" "function-edge" "-function-edge"])
 
 (enable-console-print!)
 
 (.run flows kn)
+
+(.get-or-create-index kn "know" "connected" #js ["x" "y"])
+
+(.get-or-create-index kn "remember" "str-edge" #js ["name"])
+
+(.get-or-create-index kn "forget" "str-edge" #js ["name"])
+
+(.tick flows kn)
+
+(.get-or-create-index kn "know" "edge" #js ["x" "y"])
 
 (.get-or-create-index kn "know" "connected" #js ["x" "y"])
 
