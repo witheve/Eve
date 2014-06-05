efeac9515f023528578b7a7b15f56d517acef7ed
Chris Granger <ibdknox@gmail.com>
2014-06-04 17:12:11 -0700

    remove a lot of crap
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/aurora/language.clj
+++
@@ -1,8 +0,0 @@
-(ns aurora.language)
-
-(defmacro deffact [name madlib&keys]
-  `(do
-     (def ~name
-       (fact-shape (keyword (str '~(get-in &env [:ns :name])) (str '~name)) ~madlib&keys))
-     (defn ~(symbol (str "->" name)) [& args#]
-       (fact ~name (.-arr args#)))))
--- src/aurora/other_language.cljs
+++
@@ -1,5 +0,0 @@
-(ns aurora.other-language
-  (:require [cemerick.double-check :as dc]
-            [cemerick.double-check.generators :as gen]
-            [cemerick.double-check.properties :as prop :include-macros true])
-  (:require-macros [aurora.macros :refer [apush apush* lt lte gt gte set!! dofrom]]))
