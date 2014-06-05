e93101113ed84ca23be50ebb9a7e0e8fbc2f5e1d
Chris Granger <ibdknox@gmail.com>
2014-06-04 18:15:58 -0700

    don't run incrementer on load
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/aurora/examples/incrementer.cljs
+++ src/aurora/examples/incrementer.cljs
@@ -5,20 +5,27 @@
   (:require-macros [aurora.macros :refer [typeof ainto perf-time rules]]))
 
 
-(let [program (env)]
-  (rules program
-         (rule draw-incr
-               (when "incr" {:value value})
-               (draw [:button {:id "incr" :events ["onClick"]} "increment: " 'value]))
+(defn run []
+  (let [program (env)]
+    (rules program
+           (rule draw-incr
+                 (when "incr" {:value value})
+                 (draw [:button {:id "incr" :events ["onClick"]} "increment: " 'value]))
 
-         (rule clicked
-               (when "ui/onClick" {:elem-id "incr"})
-               (func 'new-val "value + 1")
-               (change "incr" {:value 'value} {:value 'new-val}))
-         )
-  (pre-compile program)
-  (remember program "incr" ["value"] [0])
-  (know program "incr" ["value"] [0])
-  (re-run program)
-  )
+           (rule clicked
+                 (when "ui/onClick" {:elem-id "incr"})
+                 (func 'new-val "value + 1")
+                 (change "incr" {:value 'value} {:value 'new-val}))
+           )
+    (pre-compile program)
+    (remember program "incr" ["value"] [0])
+    (know program "incr" ["value"] [0])
+    (re-run program)
+    ))
 
+
+(comment
+
+  (run)
+
+ )
