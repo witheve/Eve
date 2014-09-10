3b4f1881da59ed08f168fb8633a52c0ac8ac08a8
Jamie Brandon <jamie@scattered-thoughts.net>
2014-09-10 14:17:25 -0700

    Halve all the sizes because CI is powered by hamsters

--- src/test.js
+++ src/test.js
@@ -1,29 +1,29 @@
 console.time("selfJoin");
-selfJoin.assert({maxTests: 1000});
+selfJoin.assert({maxTests: 1000, maxSize: 500});
 console.timeEnd("selfJoin");
 
 console.time("productJoin");
-productJoin.assert({maxTests: 1000, maxSize: 200});
+productJoin.assert({maxTests: 1000, maxSize: 100});
 console.timeEnd("productJoin");
 
 console.time("constantJoin");
-constantJoin.assert({maxTests: 1000});
+constantJoin.assert({maxTests: 1000, maxSize: 500});
 console.timeEnd("constantJoin");
 
 console.time("incrementalConstantJoin");
-incrementalConstantJoin.assert({maxTests: 1000});
+incrementalConstantJoin.assert({maxTests: 1000, maxSize: 500});
 console.timeEnd("incrementalConstantJoin")
 
 console.time("actualJoin");
-actualJoin.assert({maxTests: 1000, maxSize: 200});
+actualJoin.assert({maxTests: 1000, maxSize: 100});
 console.timeEnd("actualJoin");
 
 console.time("incrementalActualJoin");
-incrementalActualJoin.assert({maxTests: 1000, maxSize: 200});
+incrementalActualJoin.assert({maxTests: 1000, maxSize: 100});
 console.timeEnd("incrementalActualJoin");
 
 console.time("functionJoin");
-functionJoin.assert({maxTests: 1000, maxSize: 100});
+functionJoin.assert({maxTests: 1000, maxSize: 50});
 console.timeEnd("functionJoin");
 
 console.time("incrementalFunctionJoin");
@@ -31,15 +31,15 @@ incrementalFunctionJoin.assert({maxTests: 1000, maxSize: 50});
 console.timeEnd("incrementalFunctionJoin");
 
 console.time("negatedJoin");
-negatedJoin.assert({maxTests: 1000});
+negatedJoin.assert({maxTests: 1000, maxSize: 500});
 console.timeEnd("negatedJoin");
 
 console.time("incrementalNegatedJoin");
-incrementalNegatedJoin.assert({maxTests: 1000});
+incrementalNegatedJoin.assert({maxTests: 1000, maxSize: 500});
 console.timeEnd("incrementalNegatedJoin");
 
-console.time("Compiler tests")
+console.time("compiler tests")
 compiledPathTest();
 compiledFunctionTest();
 compiledNegationTest();
-console.timeEnd("Compiler tests");
+console.timeEnd("compiler tests");
