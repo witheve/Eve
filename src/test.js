233b1d4c50f1cc4324beb92b31b9ca25c45b3e2b
Jamie Brandon <jamie@scattered-thoughts.net>
2014-09-10 14:13:21 -0700

    Add timers to tests so that CI doesnt give up

--- src/test.js
+++ src/test.js
@@ -1,18 +1,45 @@
-console.time("Tests");
-
+console.time("selfJoin");
 selfJoin.assert({maxTests: 1000});
-productJoin.assert({maxTests: 1000, maxSize: 200});
-constantJoin.assert({maxTests: 1000});
-incrementalConstantJoin.assert({maxTests: 1000});
-actualJoin.assert({maxTests: 1000, maxSize: 200});
-incrementalActualJoin.assert({maxTests: 1000, maxSize: 200});
-functionJoin.assert({maxTests: 1000, maxSize: 100});
-incrementalFunctionJoin.assert({maxTests: 1000, maxSize: 50});
-negatedJoin.assert({maxTests: 1000});
-incrementalNegatedJoin.assert({maxTests: 1000});
+console.timeEnd("selfJoin");
 
+console.time("productJoin");
+productJoin.assert({maxTests: 1000, maxSize: 200});
+console.timeEnd("productJoin");
+
+console.time("constantJoin");
+constantJoin.assert({maxTests: 1000});
+console.timeEnd("constantJoin");
+
+console.time("incrementalConstantJoin");
+incrementalConstantJoin.assert({maxTests: 1000});
+console.timeEnd("incrementalConstantJoin")
+
+console.time("actualJoin");
+actualJoin.assert({maxTests: 1000, maxSize: 200});
+console.timeEnd("actualJoin");
+
+console.time("incrementalActualJoin");
+incrementalActualJoin.assert({maxTests: 1000, maxSize: 200});
+console.timeEnd("incrementalActualJoin");
+
+console.time("functionJoin");
+functionJoin.assert({maxTests: 1000, maxSize: 100});
+console.timeEnd("functionJoin");
+
+console.time("incrementalFunctionJoin");
+incrementalFunctionJoin.assert({maxTests: 1000, maxSize: 50});
+console.timeEnd("incrementalFunctionJoin");
+
+console.time("negatedJoin");
+negatedJoin.assert({maxTests: 1000});
+console.timeEnd("negatedJoin");
+
+console.time("incrementalNegatedJoin");
+incrementalNegatedJoin.assert({maxTests: 1000});
+console.timeEnd("incrementalNegatedJoin");
+
+console.time("Compiler tests")
 compiledPathTest();
 compiledFunctionTest();
 compiledNegationTest();
-
-console.timeEnd("Tests");
+console.timeEnd("Compiler tests");
