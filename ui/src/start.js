48843a81ab39749cc92d04ebf5d868f4a8adfd9a
Chris Granger <ibdknox@gmail.com>
2015-03-16 23:07:50 -0700

    add indexer, draw table from indexes, add dispatches, include uuid.js
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- ui/src/start.js
+++ ui/src/start.js
@@ -2,6 +2,9 @@
 // Utils
 //---------------------------------------------------------
 
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 function reactFactory(obj) {
   return React.createFactory(React.createClass(obj));
 }
@@ -11,6 +14,7 @@ function reactFactory(obj) {
 //---------------------------------------------------------
 
 var uiState = {};
+var ixer = new Indexing.Indexer();
 
 //---------------------------------------------------------
 // Root component
@@ -18,8 +22,8 @@ var uiState = {};
 
 var root = reactFactory({
   render: function() {
-    return JSML(["p", "hey!"]);
-//     return table({fields: ["no wai", "ya wai"], rows: [[0,1],[0,2]]});
+//     return JSML(["p", "hey!"]);
+    return table({table: "foo"});
   }
 });
 
@@ -44,6 +48,9 @@ var editable = reactFactory({
       this.submit();
       e.preventDefault();
     }
+    if(this.props.onKey) {
+      this.props.onKey(e);
+    }
   },
   submit: function() {
     if(this.props.onSubmit && this.state.modified) {
@@ -57,14 +64,14 @@ var editable = reactFactory({
                          "onInput": this.handleChange,
                          "onBlur": this.submit,
                          "onKeyDown": this.handleKeys,
-                         dangerouslySetInnerHTML: {__html: value}}]);
+                         dangerouslySetInnerHTML: {__html: value !== undefined ? value : ""}}]);
   }
 });
 
 var tableHeader = reactFactory({
   renameHeader: function(value) {
     //do stuff
-    dispatch("rename", {id: this.props.field, value: value});
+    dispatch("rename", {id: this.props.id, value: value});
   },
   render: function() {
     return JSML(["th", editable({value: this.props.field, onSubmit: this.renameHeader})]);
@@ -73,18 +80,43 @@ var tableHeader = reactFactory({
 
 var tableRow = reactFactory({
   getInitialState: function() {
-    return {row: []};
+    var row = [];
+    var cur = this.props.row;
+    for(var i = 0, len = this.props.length; i < len; i++) {
+      row[i] = cur[i];
+    }
+    return {row: row};
+  },
+  componentDidUpdate: function() {
+    var row = this.state.row;
+    var cur = this.props.row;
+    for(var i = 0, len = this.props.length; i < len; i++) {
+      row[i] = cur[i];
+    }
+  },
+  checkRowComplete: function() {
+    var row = this.state.row;
+    for(var i = 0, len = this.props.length; i < len; i++) {
+      if(row[i] === undefined) return false;
+    }
+    return true;
   },
   setColumn: function(ix, value) {
     this.state.row[ix] = value;
-    console.log(this.state.row);
+    if(this.checkRowComplete()) {
+      this.submitRow();
+    }
   },
   submitRow: function() {
-    dispatch("setRow", {table: this.props.table, row: this.props.row, updatedRow: this.state.row});
+    if(this.props.isNewRow) {
+      dispatch("addRow", {table: this.props.table, neue: this.state.row});
+    } else {
+      dispatch("swapRow", {table: this.props.table, old: this.props.row, neue: this.state.row});
+    }
   },
   render: function() {
     var self = this;
-    var fields = this.props.row.map(function(field, ix) {
+    var fields = this.state.row.map(function(field, ix) {
       return ["td", editable({value: field, onSubmit: function(value) {
         self.setColumn(ix, value);
       }})];
@@ -94,16 +126,26 @@ var tableRow = reactFactory({
 });
 
 var table = reactFactory({
+  addColumn: function() {
+    dispatch("addColumnToTable", {table: this.props.table});
+  },
   render: function() {
-    var headers = this.props.fields.map(function(cur) {
-      return tableHeader({field: cur});
+    var self = this;
+    var fields = code.viewToFields(this.props.table);
+    var rows = ixer.facts(this.props.table);
+    var numColumns = fields.length;
+    var headers = fields.map(function(cur) {
+      return tableHeader({field: code.name(cur[0]), id: cur[0]});
     });
-    var rows = this.props.rows.map(function(cur) {
-      return tableRow({row: cur});
+    var rows = rows.map(function(cur) {
+      return tableRow({table: self.props.table, row: cur, length: numColumns});
     });
-    return JSML(["table",
-                ["thead", ["tr", headers]],
-                ["tbody", rows]]);
+    rows.push(tableRow({table: this.props.table, row: [], length: numColumns, isNewRow: true}));
+    return JSML(["div", {className: "tableWrapper"},
+                 ["table",
+                  ["thead", ["tr", headers]],
+                  ["tbody", rows]],
+                ["div", {className: "addColumn", onClick: this.addColumn}, "+"]]);
   }
 });
 
@@ -123,6 +165,25 @@ function dispatch(event, arg, noRedraw) {
   switch(event) {
     case "load":
       break;
+    case "addColumnToTable":
+      var diffs = code.diffs.addColumn(arg.table);
+      ixer.handleDiffs(diffs);
+      break;
+    case "swapRow":
+      var diffs = {};
+      diffs[arg.table] = {adds: [arg.neue], removes: [arg.old]};
+      ixer.handleDiffs(diffs);
+      break;
+    case "addRow":
+      var diffs = {};
+      diffs[arg.table] = {adds: [arg.neue], removes: []};
+      ixer.handleDiffs(diffs);
+      break;
+    case "rename":
+      console.log("rename", arg);
+      var diffs = code.diffs.changeDisplayName(arg.id, arg.value);
+      ixer.handleDiffs(diffs);
+      break;
     default:
       console.error("Dispatch for unknown event: ", event, arg);
       break;
@@ -133,8 +194,57 @@ function dispatch(event, arg, noRedraw) {
   }
 }
 
+
+//---------------------------------------------------------
+// Data API
+//---------------------------------------------------------
+
+var code = {
+  diffs: {
+    addColumn: function(viewId) {
+      var view = ixer.index("view")[viewId];
+      var fields = code.viewToFields(viewId) || [];
+      var schema = view[1];
+      var fieldId = uuid();
+      var diffs = {
+        field: {adds: [[fieldId, schema, fields.length, "unknown"]], removes: []},
+        displayName: {adds: [[fieldId, alphabet[fields.length]]], removes: []}
+      };
+      return diffs;
+    },
+    changeDisplayName: function(id, neue) {
+      var cur = ixer.index("displayName")[id];
+      var diffs = {
+        displayName: {adds: [[id, neue]], removes: [[id, cur]]}
+      }
+      return diffs;
+    }
+  },
+  viewToFields: function(view) {
+    var schema = ixer.index("viewToSchema")[view];
+    return ixer.index("schemaToFields")[schema];
+  },
+  name: function(id) {
+    return ixer.index("displayName")[id];
+  }
+}
+
 //---------------------------------------------------------
 // Go
 //---------------------------------------------------------
+
+
+//add some views
+ixer.addIndex("displayName", "displayName", Indexing.create.lookup([0, 1]));
+ixer.addIndex("view", "view", Indexing.create.lookup([0, false]));
+ixer.addIndex("viewToSchema", "view", Indexing.create.lookup([0, 1]));
+ixer.addIndex("schemaToFields", "field", Indexing.create.collector([1]));
+ixer.handleDiffs({view: {adds: [["foo", "foo-schema", false]], removes: []},
+                  schema: {adds: [["foo-schema"]], removes: []},
+                  field: {adds: [["foo-a", "foo-schema", 0, "string"], ["foo-b", "foo-schema", 1, "string"]], removes: []},
+                  foo: {adds: [["a", "b"], ["c", "d"]], removes: []},
+                  input: {adds: [["foo", ["a", "b"]], ["foo", ["c", "d"]]], removes: []},
+                  displayName: {adds: [["foo-a", "foo A"], ["foo-b", "foo B"]], removes: []},
+                 });
 
 dispatch("load");
