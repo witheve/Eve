38a81e81c56a1238f247abc9e0f72650ca378784
Joshua Cole <joshuafcole@gmail.com>
2015-03-17 16:55:10 -0700

    Adds Grid.findGap and Grid.hasGapAt

--- ui/src/grid.js
+++ ui/src/grid.js
@@ -14,6 +14,15 @@ var Grid = (function(document, React, Velocity) {
     NO_SIZE: "Must provide a valid size in grid cells."
   };
 
+  function make2DArray(width, height, defaultValue) {
+    var arr = [];
+    while(arr.push([]) < width) {
+      var col = arr[arr.length - 1];
+      while(col.push(defaultValue) < height) {}
+    }
+    return arr;
+  }
+
   return {
     DEFAULT_SIZE: [6, 3],
     makeGrid: function makeGrid(params) { // (Any) -> Grid
@@ -63,21 +72,60 @@ var Grid = (function(document, React, Velocity) {
       };
       return rect;
     },
-    evacuateRect: function evacuateRect(grid, pos, size, from) { // (Grid, Pos, Size, Pos) -> Rect
+    evacuateRect: function evacuateRect(grid, pos, size, from) { // (Grid, Pos, Size, Pos?) -> Rect
       if(!grid) { throw new Error(ERR.NO_GRID); }
       if(!pos) { throw new Error(ERR.NO_POS); }
       if(!size) { throw new Error(ERR.NO_SIZE); }
       from = from || [grid.size[0] / 2, grid.size[1] / 2];
     },
-    confineRect: function confineRect(grid, pos, size, to) { // (Grid, Pos, Size, Pos) -> Rect
+    confineRect: function confineRect(grid, pos, size, to) { // (Grid, Pos, Size, Pos?) -> Rect
       if(!grid) { throw new Error(ERR.NO_GRID); }
       if(!pos) { throw new Error(ERR.NO_POS); }
       if(!size) { throw new Error(ERR.NO_SIZE); }
       to = to || [grid.size[0] / 2, grid.size[1] / 2]; // @NOTE: I'm not sure this default makes sense.
     },
-    findGap: function findGap(grid, size) { // (Grid, Size) -> Size?
+    tilesToMap: function tilesToMap(grid, tiles) { // (Grid, Tile[]) -> TileMap
+      var map = make2DArray(grid.size[0], grid.size[1], 0);
+      for(var ix = 0, len = tiles.length; ix < len; ix++) {
+        var tile = tiles[ix];
+        for(var x = tile.pos[0]; x < tile.pos[0] + tile.size[0]; x++) {
+          for(var y = tile.pos[1]; y < tile.pos[1] + tile.size[1]; y++) {
+            map[x][y]++;
+          }
+        }
+      }
+      return map;
+    },
+    hasGapAt: function hasGapAt(grid, tiles, pos, size, map) { // (Grid, Tile[], Pos, Size, TileMap?) -> Bool
+      if(!grid) { throw new Error(ERR.NO_GRID); }
+      if(!pos) { throw new Error(ERR.NO_POS); }
+      if(!size) { throw new Error(ERR.NO_SIZE); }
+      if(!map) { map = Grid.tilesToMap(grid, tiles); }
+
+      if(pos[0] + size[0] > grid.size[0]) { return false; }
+      if(pos[1] + size[1] > grid.size[1]) { return false; }
+
+      for(var x = pos[0]; x < pos[0] + size[0]; x++) {
+        for(var y = pos[1]; y < pos[1] + size[1]; y++) {
+          if(map[x][y] > 0) {
+            return false;
+          }
+        }
+      }
+      return true;
+    },
+    findGap: function findGap(grid, tiles, size) { // (Grid, Tile[], Size) -> Pos?
       if(!grid) { throw new Error(ERR.NO_GRID); }
       if(!size) { throw new Error(ERR.NO_SIZE); }
+      var map = Grid.tilesToMap(grid, tiles);
+      for(var x = 0; x <= grid.size[0] - size[0]; x++) {
+        for(var y = 0; y <= grid.size[1] - size[1]; y++) {
+          if(map[x][y] > 0) { continue; }
+          if(Grid.hasGapAt(grid, tiles, [x, y], size, map)) {
+            return [x, y];
+          }
+        }
+      }
     }
   };
 })(window.document, React, Velocity);
