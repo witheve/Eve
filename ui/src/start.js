ab3f004be7f8261e958336899dd24298a0d23c8e
Chris Granger <ibdknox@gmail.com>
2015-03-16 17:19:11 -0700

    dispatch, root element, jsml
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- ui/src/start.js
+++ ui/src/start.js
@@ -1,1 +1,21 @@
-React.render(React.createElement("p", {}, "Hey!"), document.body);
+var root = React.createClass({
+  render: function() {
+    return JSML(["p", "hey!"]);
+  }
+});
+
+function dispatch(event, arg, noRedraw) {
+  switch(event) {
+    case "load":
+      break;
+    default:
+      console.error("Dispatch for unknown event: ", event, arg);
+      break;
+  }
+
+  if(!noRedraw) {
+    React.render(React.createElement(root), document.body);
+  }
+}
+
+dispatch("load");
