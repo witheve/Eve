713707098c5ed7e304b8ac41ef2602f0d66e6529
Jamie Brandon <jamie@scattered-thoughts.net>
2015-04-22 17:16:21 -0700

    Set correct default version of rust

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 pushd .
   cd runtime
-  RUST_BACKTRACE=1 multirust run nightly-2015-04-20 cargo run --bin=server --release
+  multirust override nightly-2015-04-20
+  RUST_BACKTRACE=1 cargo run --bin=server --release
 popd
