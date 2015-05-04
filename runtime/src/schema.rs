e9e25e27f83393d6aec70b04549d67c0e51c210f
Jamie Brandon <jamie@scattered-thoughts.net>
2015-05-04 15:24:38 -0700

    Progress on ast

--- runtime/src/schema.rs
+++ runtime/src/schema.rs
@@ -2,62 +2,53 @@ use value::Value;
 
 type Id = String;
 
-// K is the unique key
+// a table with a single unique key K
 type KV<K, V> = Hashtable<K, V>;
 
-// two columns - no unique keys
-type VV<K,V> = Hashtable<K, V>;
-
-// two columns - first two are the unique key
-type KKV<K1, K2, V> = Hashtable<K1, Hashtable<K2, V>>;
-
+// every record in this struct represents a table
+// the tables are named by their fields, separated by underscores
+// eg view_kind is a table with two fields: view and kind
 type Program = {
     // all state lives in a view of some kind
     view_kind: KV<Id, ViewKind>,
 
-    // views have fields, fields may belong to multiple views
-    field_view: VV<Id, Id>,
+    // views have fields which are globally unique
+    field_view: KV<Id, Id>,
+    // some fields have constraints on how they can be queried
+    field_kind: KV<Id, FieldKind>,
 
     // a table has an insert view and a remove view
     table_insert: KV<Id, Id>,
     table_remove: KV<Id, Id>,
 
-    // a query just indirects to another view
-    query_view: KV<Id, Id>,
-
-    // sources uniquely identify a copy of a child view inside a parent view
+    // sources uniquely identify a copy of a child view inside a parent join or union
     // eg a parent join can have multiple copies of some child view, with different source ids
-    source_parent: KV<Id, Id>,
-    source_child: KV<Id, Id>,
+    parent_source_child: KV<(Id, Id), Id>,
+    // every view also has an implicit "constant" source
+    // anywhere a source-field pair is expected, you can instead use "constant"-id
+    constant_value: KV<Id, Value>,
 
     // constraints belong to a join view
-    // the left and right are references which are compared using the operation
+    // the left and right fields are compared using the operation
     constraint_parent: KV<Id, Id>,
-    constraint_left: KV<Id, Id>,
+    constraint_leftsource_leftfield: KV<Id, (Id, Id)>,
     constraint_operation: KV<Id, ConstraintOperation>,
-    constraint_right: KV<Id, Id>,
+    constraint_rightsource_rightfield: KV<Id, (Id, Id)>,
 
-    // aggregates group an inner view by the rows of an outer view
-    // the groups are reduced using some primitive
-    aggregate_primitive: KV<Id, Id>,
-    aggregate_outer: KV<Id, Id>,
-    aggregate_inner: KV<Id, Id>,
+    // aggregates group an "inner" source by the rows of an "outer" source
+    // the groups are reduced by joining against a "primitive" source
+    // the grouping is determined by binding inner fields to outer fields or constants
+    aggregate_innerfield_source_sourcefield: KV<(Id, Id), (Id, Id)>,
+    // inner fields and constants can also be used as arguments to the aggregate function
+    aggregate_primitivefield_source_sourcefield: KV<(Id, Id), (Id, Id)>,
 
-    // the aggregate grouping is determined by binding outer fields to inner fields
-    // bound fields can optionally be passed to the aggregate primitive as arguments
-    aggregate_outerfield_binding: KKV<Id, Id, Id>,
-    binding_innerfield: KV<Id, Id>,
-    binding_primitivefield: KV<Id, ID>,
-
-    // primitives are used to access built-in operations
-    primitive: K<Id>,
-
-    // constant references just refer to some value
-    reference_value: KV<Id, Value>,
-
-    // field references refer to a field in either a source (for join) or TODO
-    reference_field: KV<Id, Id>,
-    reference_source: KV<Id, Id>,
+    // views produce output by binding fields from sources
+    // each join field is bound exactly once
+    join_joinfield_source_sourcefield: KV<(Id, Id), (Id, Id)>,
+    // each union field is bound exactly once per source
+    union_unionfield_source_sourcefield: KV<(Id, Id, Id), Id>,
+    // each aggregate field is bound exactly once and cannot bind "outer"
+    aggregate_aggregatefield_source_sourcefield: KV<(Id, Id), (Id, Id)>,
 
     // things that live in an ordered list are sorted by some f64
     thing_order: KV<Id, f64>,
@@ -65,19 +56,23 @@ type Program = {
     // things can have human readable names
     thing_name: KV<Id, String>,
 
-    // TODO functions
-    // TODO function and aggregate arguments (FieldKind?)
-    // TODO select
-    // TODO union
-    // TODO negate
+    // TODO primitives cannot represent limit
+    // TODO want multiple primitives per aggregate?
+    // TODO sort order for aggregates?
 }
 
 enum ViewKind {
     Table, // a view which can depend on the past
-    Query, // a wrapper around some workspace full of views
     Join, // take the product of multiple views and filter the results
     Union, // take the union of multiple views
     Aggregate, // group one view by the contents of another and run some primitive on the groups
+    Primitive, // a built-in function, represented as a view with one or more non-Data fields
+}
+
+enum FieldKind {
+    Data, // a normal field
+    ScalarInput, // a field that must be constrained to a single scalar value
+    VectorInput, // a field that must be constrained to a single vector value (in an aggregate)
 }
 
 enum ConstraintOperation {
@@ -87,9 +82,4 @@ enum ConstraintOperation {
     LessThanOrEqual,
     GreaterThan,
     GreaterThanOrEqual,
-}
-
-enum PrimitiveKind {
-    Function,
-    Aggregate,
 }
