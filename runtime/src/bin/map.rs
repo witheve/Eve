7cd74e813053b9ce30133d987be413653ea0b081
Jamie Brandon <jamie@scattered-thoughts.net>
2015-05-12 19:35:25 -0700

    Really dumb benchmark

--- runtime/src/bin/map.rs
+++ runtime/src/bin/map.rs
@@ -1,6 +1,32 @@
+#![feature(test)]
+
+extern crate test;
 extern crate eve;
 
+use test::Bencher;
+use std::collections::BTreeMap;
+
 use eve::map::*;
+
+#[bench]
+fn bench_insert_map(b: &mut Bencher) {
+    b.iter(|| {
+        let mut map = Map::new();
+        for i in (0..1000000) {
+            map.insert(i, 1);
+        }
+    });
+}
+
+#[bench]
+fn bench_insert_btree(b: &mut Bencher) {
+    b.iter(|| {
+        let mut map = BTreeMap::new();
+        for i in (0..1000000) {
+            map.insert(i, 1);
+        }
+    });
+}
 
 #[allow(dead_code)]
 fn main() {
