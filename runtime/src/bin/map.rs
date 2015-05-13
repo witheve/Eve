3557194f06fc28817cc75a8e8a95581b8c39795d
Jamie Brandon <jamie@scattered-thoughts.net>
2015-05-13 16:15:10 -0700

    Improve benchmarks

--- runtime/src/bin/map.rs
+++ runtime/src/bin/map.rs
@@ -5,35 +5,76 @@ extern crate eve;
 
 use test::Bencher;
 use std::collections::BTreeMap;
+use std::io::prelude::*;
+use std::fs::File;
 
 use eve::map::*;
 
 #[bench]
-fn bench_insert_map_3(b: &mut Bencher) {
+fn bench_insert_int_map_3(b: &mut Bencher) {
     b.iter(|| {
         let mut map = Map::new(3);
-        for i in (0..1000000) {
-            map.insert(i, 1);
+        for i in (0i64..1000000) {
+            map.insert(i * 25214903917, 1);
         }
     });
 }
 
 #[bench]
-fn bench_insert_map_1(b: &mut Bencher) {
+fn bench_insert_int_map_1(b: &mut Bencher) {
     b.iter(|| {
         let mut map = Map::new(1);
-        for i in (0..1000000) {
-            map.insert(i, 1);
+        for i in (0i64..1000000) {
+            map.insert(i * 25214903917, 1);
         }
     });
 }
 
 #[bench]
-fn bench_insert_btree(b: &mut Bencher) {
+fn bench_insert_int_btree(b: &mut Bencher) {
     b.iter(|| {
         let mut map = BTreeMap::new();
-        for i in (0..1000000) {
-            map.insert(i, 1);
+        for i in (0i64..1000000) {
+            map.insert(i * 25214903917, 1);
+        }
+    });
+}
+
+fn words() -> Vec<String> {
+    let mut contents = String::new();
+    File::open("/usr/share/dict/words").unwrap().read_to_string(&mut contents).unwrap();
+    contents.lines().map(|word| word.to_owned()).collect()
+}
+
+#[bench]
+fn bench_insert_string_map_3(b: &mut Bencher) {
+    let words = words();
+    b.iter(|| {
+        let mut map = Map::new(3);
+        for word in words.iter() {
+            map.insert(word.clone(), 1);
+        }
+    });
+}
+
+#[bench]
+fn bench_insert_string_map_1(b: &mut Bencher) {
+    let words = words();
+    b.iter(|| {
+        let mut map = Map::new(1);
+        for word in words.iter() {
+            map.insert(word.clone(), 1);
+        }
+    });
+}
+
+#[bench]
+fn bench_insert_string_btree(b: &mut Bencher) {
+    let words = words();
+    b.iter(|| {
+        let mut map = BTreeMap::new();
+        for word in words.iter() {
+            map.insert(word.clone(), 1);
         }
     });
 }
