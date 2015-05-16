49b42c162239d34640548f2fa17dd40183cfc5fa
Joshua Cole <joshuafcole@gmail.com>
2015-05-15 18:57:54 -0700

    Add --debug flag

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 pushd .
   cd runtime
   multirust override nightly-2015-05-14
-  RUST_BACKTRACE=1 cargo run --bin=server --release
+  if [ "x$1" = "x--debug" ]; then
+    RUST_BACKTRACE=1 cargo run --bin=server
+  else
+    RUST_BACKTRACE=1 cargo run --bin=server --release
+  fi
 popd
