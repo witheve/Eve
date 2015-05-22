aaefd43687c12aa27ad769cb542352dc7dc0e02b
cmontella <cmontella@ubuntu>
2015-05-22 13:39:12 -0700

    Added a "clean" command line option to bin/server that starts the server in with an empty events log.

--- runtime/src/bin/server.rs
+++ runtime/src/bin/server.rs
@@ -1,8 +1,21 @@
 extern crate eve;
 
+use std::env;
 use eve::server::*;
+use std::fs::OpenOptions;
 
 #[allow(dead_code)]
 fn main() {
+
+	for argument in env::args() {
+    	match &*argument {
+    		"clean" => {
+    			OpenOptions::new().create(true).truncate(true).open("./events").unwrap();
+    			()
+    		},
+    		_ => continue,
+    	}
+	}
+
     run()
 }
