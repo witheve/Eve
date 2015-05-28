6dd7b2dba310919d98383559a649f6b2f8eecb82
Jamie Brandon <jamie@scattered-thoughts.net>
2015-05-27 18:32:21 -0700

    Add a test

--- runtime/src/bin/server.rs
+++ runtime/src/bin/server.rs
@@ -1,8 +1,23 @@
 extern crate eve;
 
 use std::env;
-use eve::server::*;
 use std::fs::OpenOptions;
+
+use eve::server::*;
+use eve::value::*;
+
+#[test]
+fn test_add() {
+    let flow = load("tests/add-events");
+    assert_eq!(
+        flow.get_output("2e0bc048-c3c6-4170-a0b6-61edee68ee12").index.iter().collect::<Vec<_>>(),
+        vec![
+            &vec![Value::Float(2.0)],
+            &vec![Value::Float(4.0)],
+            &vec![Value::Float(6.0)],
+        ]
+    );
+}
 
 #[allow(dead_code)]
 fn main() {
