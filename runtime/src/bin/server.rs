9bf1c9d7ffce2a50b0efc9f276225802596bfe55
cmontella <cmontella@live.com>
2015-06-04 12:07:22 -0700

    Fix warnings

--- runtime/src/bin/server.rs
+++ runtime/src/bin/server.rs
@@ -1,15 +1,10 @@
-#![feature(fs_walk)]
-
 extern crate rustc_serialize;
 extern crate eve;
 
 use std::env;
-use std::fs::{OpenOptions, walk_dir};
-use std::io::prelude::*;
-use rustc_serialize::json::Json;
+use std::fs::OpenOptions;
 
 use eve::server::*;
-use eve::value::*;
 
 #[test]
 fn test_examples() {
