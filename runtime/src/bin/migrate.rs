71e55d1b1e95b2892a907890ebc01b2482d5d4a8
Jamie Brandon <jamie@scattered-thoughts.net>
2015-06-30 17:23:03 -0700

    Add compact command to migrate

--- runtime/src/bin/migrate.rs
+++ runtime/src/bin/migrate.rs
@@ -1,5 +1,6 @@
 #![feature(fs_walk)]
 #![feature(slice_patterns)]
+#![feature(drain)]
 
 extern crate rustc_serialize;
 extern crate eve;
@@ -10,53 +11,59 @@ use std::io::prelude::*;
 use rustc_serialize::json::{ToJson, Json};
 
 use eve::server::*;
-use eve::value::*;
+use eve::flow::*;
 
-fn migrate_file<F>(filename: String, migrate: &F) where F: Fn(&mut Event) {
-    let mut new_events = Vec::new();
+fn migrate_file<F>(filename: &str, migrate: &F) where F: Fn(&mut Vec<Event>) {
+    let mut old_events_string = String::new();
     {
         let mut old_events_file = OpenOptions::new().create(true).open(&filename).unwrap();
-        let mut old_events = String::new();
-        old_events_file.read_to_string(&mut old_events).unwrap();
-        for line in old_events.lines() {
-            let json = Json::from_str(&line).unwrap();
-            let mut event: Event = FromJson::from_json(&json);
-            migrate(&mut event);
-            new_events.push(event);
-        }
+        old_events_file.read_to_string(&mut old_events_string).unwrap();
     }
+    let mut events = old_events_string.lines().map(|line|
+        FromJson::from_json(&Json::from_str(&line).unwrap())
+        ).collect();
+    migrate(&mut events);
     let mut new_events_file = OpenOptions::new().truncate(true).write(true).open(&filename).unwrap();
-    for event in new_events {
+    for event in events {
         new_events_file.write_all(format!("{}", event.to_json()).as_bytes()).unwrap();
         new_events_file.write_all("\n".as_bytes()).unwrap();
     }
 }
 
-#[allow(dead_code)]
-fn migrate_all<F>(migrate: F) where F: Fn(&mut Event) {
+fn migrate_all<F>(migrate: &F) where F: Fn(&mut Vec<Event>) {
     for entry in walk_dir("./test-inputs").unwrap() {
         let filename = entry.unwrap().path().to_str().unwrap().to_owned();
-        migrate_file(filename, &migrate)
+        migrate_file(&filename[..], &migrate)
     }
     for entry in walk_dir("./examples").unwrap() {
         let filename = entry.unwrap().path().to_str().unwrap().to_owned();
-        migrate_file(filename, &migrate)
+        migrate_file(&filename[..], &migrate)
     }
-    migrate_file("./events".to_string(), &migrate)
+    migrate_file("./events", &migrate);
+    migrate_file("./bootstrap", &migrate);
 }
 
-#[allow(dead_code)]
-fn remove_view(id: &str) {
-    migrate_all(|event| {
+fn remove_view(id: &str, events: &mut Vec<Event>) {
+    for event in events.iter_mut() {
         event.changes.retain(|&(ref change_id, _)| change_id != id);
-    });
+    }
+}
+
+fn compact(events: &mut Vec<Event>) {
+    let mut flow = Flow::new();
+    for event in events.drain(..) {
+        flow = flow.quiesce(event.changes);
+    }
+    // TODO session is blank which doesn't seem to matter because it is never used
+    events.push(Event{changes: flow.as_changes(), session: "".to_owned()})
 }
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let borrowed_args = args.iter().map(|s| &s[..]).collect::<Vec<&str>>();
     match &borrowed_args[..] {
-        [_, "remove_view", id] => remove_view(id),
+        [_, "remove_view", id] => migrate_all(&|events| remove_view(id, events)),
+        [_, "compact", filename] => migrate_file(filename, &compact),
         other => panic!("Bad arguments (look at src/bin/migrate.rs for correct usage): {:?}", &other[1..]),
     }
 }
