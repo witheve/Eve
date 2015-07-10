0c4fa2dd79da0fb52b340f2caa47cd91401ffd3c
cmontella <cmontella@live.com>
2015-07-10 15:55:40 -0700

    Added reverse geolocation to get user's city from lat-lon. Updated location table in compiler and scrubbed 5SQ events file for compatibility.

--- ui/src/eveApp.ts
+++ ui/src/eveApp.ts
@@ -8,11 +8,11 @@ module eveApp {
   var renderer = uiEditorRenderer;
   var ixer = api.ixer;
   document.body.appendChild(renderer.root);
-  
+
   //---------------------------------------------------------
   // Session url
   //---------------------------------------------------------
-  
+
   function reportCurrentUrl() {
     var loc = window.location;
     //["session url", "inserted", [renderer.session, renderer.nextEventId(), loc.href, loc.origin, loc.pathname, loc.hash]]
@@ -28,31 +28,49 @@ module eveApp {
     diffs.push(api.remove("session url", {session: renderer.session}));
     client.sendToServer(api.toDiffs(diffs), false);
   }
-  
+
   //---------------------------------------------------------
   // Geo Location
   //---------------------------------------------------------
-    
+
   function handlePosition(pos) {
+
     var coords = pos.coords;
     var diffs = [];
-    diffs.push(api.insert("location", {
-      session: renderer.session,
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      accuracy: coords.accuracy,
-      timestamp: pos.timestamp
-    }));
-    diffs.push(api.remove("location", {session: renderer.session}));
-    client.sendToServer(api.toDiffs(diffs), false);
+
+    // Get the reverse geolocation
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coords.latitude +"," + coords.longitude + "&key=AIzaSyAvM1xyws3JDenRLdZkPjGjCrF1mlf2tGA"; // registered w/ Corey
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            var response = xhr.responseText;
+            var address = JSON.parse(response);
+            var city = address.results[0].address_components[3].long_name;
+
+            diffs.push(api.insert("location", {
+              session: renderer.session,
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+              accuracy: coords.accuracy,
+              timestamp: pos.timestamp,
+              city: city
+            }));
+            diffs.push(api.remove("location", {session: renderer.session}));
+            client.sendToServer(api.toDiffs(diffs), false);
+        }
+    }
+    xhr.open('GET',url, true);
+    xhr.send(null);
   }
-  
+
   //---------------------------------------------------------
   // Set up init
   //---------------------------------------------------------
-  
+
   client.afterInit(() => {
-    reportCurrentUrl();  
+    reportCurrentUrl();
     navigator.geolocation.watchPosition(handlePosition);
   });
 
