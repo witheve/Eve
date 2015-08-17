9f9765f891778fcae2d1c1d060524d79963a8dfb
Steve Klabnik <steve@steveklabnik.com>
2015-08-17 18:08:29 -0400

    Add some resilience to run.sh
    1. This needs bash, not just regular sh. This was true before these
       changes, but also you should only use hash to test for program
       existence if you're using bash[1]
    2. Test for npm and multirust appropriately.
    
    1: http://stackoverflow.com/a/677212/24817

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 waitUrl="`pwd`/ui/waiting-room.html";
+
+hash npm 2>/dev/null || { echo >&2 "I require npm but it's not installed. Aborting."; exit 1; }
 
 pushd .
   # Ensure typescript compiler is present and compile UI
@@ -8,8 +10,13 @@ pushd .
   if hash tsc 2>/dev/null; then
     tsc
   else
-    npm install -g typescript
-    tsc
+    if hash npm 2>/dev/null; then
+      npm install -g typescript
+      tsc
+    else
+      echo >&2 "I require typescript, but it's not installed. Please install tsc itself or npm and this script will install tsc for you. Aborting."
+      exit 1;
+    fi
   fi
 popd
 
@@ -22,13 +29,19 @@ if [[ "x$1" != "x--restart" ]]; then
   fi
 fi
 
-pushd .
-  # Ensure rustc is updated and compile backend
-  cd runtime
-  multirust override nightly-2015-08-10
-  if [[ "x$1" == "x--debug" ]]; then
-    RUST_BACKTRACE=1 cargo run --bin=server
-  else
-    RUST_BACKTRACE=1 cargo run --bin=server --release
-  fi
-popd
+if hash multirust 2>/dev/null; then
+  pushd .
+    # Ensure rustc is updated and compile backend
+    cd runtime
+    multirust override nightly-2015-08-10
+    if [[ "x$1" == "x--debug" ]]; then
+      RUST_BACKTRACE=1 cargo run --bin=server
+    else
+      RUST_BACKTRACE=1 cargo run --bin=server --release
+    fi
+  popd
+else
+  echo >&2 "I require multirust, but it's not installed. Aborting."
+  exit 1;
+fi
+
