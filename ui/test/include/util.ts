386035d15236843e53d5844f99a7f50c2faaa184
Joshua Cole <joshuafcole@gmail.com>
2015-09-01 13:04:18 -0700

    Abstract item type, add item subtypes, and preserve type information using typeof

--- ui/test/include/util.ts
+++ ui/test/include/util.ts
@@ -4,7 +4,7 @@
 module Test {
   export interface CasperUtil extends Casper {
     __env?:any
-    __util
+    __util:typeof util
   }
 
   export type Selector = string;
@@ -46,7 +46,51 @@ module Test {
     return casper;
   }
 
-  let util = casper.__util = {
+  function item<T extends {}>(prefixKind:string|string[] = [], extra:T = <T>{}) {
+    let prefixKinds = [].concat(prefixKind);
+    let util = {
+      select(kind:string|string[] = [], ix?:number, within?:string):Selector {
+        let kinds:string[] = prefixKinds.concat(kind);
+        let semantic = (kinds && kinds.length) ? `[data-semantic^="item::${kinds.join("::")}"]` : `[data-semantic^="item"]`;
+        let nthChild = (ix !== undefined) ? `:nth-child(${ix})` : "";
+        return (within ? within + " " : "") + semantic + nthChild;
+      },
+      count(kind?:string|string[], within?:string):number {
+        let sel = this.select(kind, undefined, within);
+        if(!casper.exists(sel)) { return 0; }
+        let elems = casper.getElementsBounds(sel);
+        return elems.length;
+      },
+      selection(kind?:string, ix?:number, within?:string):Selector {
+        return this.select(kind, ix, within) + ".selected";
+      },
+      selectionCount(kind?:string, within?:string):number {
+        let sel = this.selection(kind, undefined, within);
+        if(!casper.exists(sel)) { return 0; }
+        let elems = casper.getElementsAttribute(sel, "class");
+        return elems.length;
+      },
+      selected(kind?:string, ix?:number, within?:string):boolean {
+        let sel = this.select(kind, ix, within);
+        if(!casper.exists(sel)) { throw new Error(`Item ${kind} ${ix} does not exist, cannot be selected.`); }
+        let classes = casper.getElementsAttribute(sel, "class");
+        for(let klass of classes) {
+          if(klass.indexOf("selected") === -1) {
+            return false;
+          }
+        }
+        return true;
+      }
+    };
+    for(let key in util) {
+      if(!extra[key]) {
+        extra[key] = util[key];
+      }
+    }
+    return <T & typeof util> extra;
+  }
+
+  var util = {
     action: {
       select(name:string):Selector {
         return `[data-semantic="action::${name}"]`;
@@ -62,50 +106,21 @@ module Test {
         return `[data-semantic="pane::${name}"]`;
       }
     },
-    item: {
-      select(kind?:string|string[], ix?:number, within?:string):Selector {
-        let kinds:string[] = (typeof kind === "string") ? [kind] : kind;
-        let semantic = (kinds && kinds.length) ? `[data-semantic^="item::${kinds.join("::")}"]` : `[data-semantic^="item"]`;
-        let nthChild = (ix !== undefined) ? `:nth-child(${ix})` : "";
-        return (within ? within + " " : "") + semantic + nthChild;
-      },
-      count(kind?:string, within?:string):number {
-        let sel = util.item.select(kind, undefined, within);
-        if(!casper.exists(sel)) { return 0; }
-        let elems = casper.getElementsBounds(sel);
-        return elems.length;
-      },
-      selection(kind?:string, ix?:number, within?:string):Selector {
-        return util.item.select(kind, ix, within) + ".selected";
-      },
-      selectionCount(kind?:string, within?:string):number {
-        let sel = util.item.selection(kind, undefined, within);
-        if(!casper.exists(sel)) { return 0; }
-        let elems = casper.getElementsAttribute(sel, "class");
-        return elems.length;
-      },
-      selected(kind?:string, ix?:number, within?:string):boolean {
-        let sel = util.item.select(kind, ix, within);
-        if(!casper.exists(sel)) { throw new Error(`Item ${kind} ${ix} does not exist, cannot be selected.`); }
-        let classes = casper.getElementsAttribute(sel, "class");
-        for(let klass of classes) {
-          if(klass.indexOf("selected") === -1) {
-            return false;
-          }
-        }
-        return true;
-      }
-    },
+    item: item(undefined, {
+      tab: item("tab"),
+      card: item("card")
+    }),
 
     // Interactions
     shiftClick,
 
     // Assertions
-    assertActions(actions:string[], enabled:{[action:string]: boolean}, test:Tester) {
+    assertActions(actions:string[], enabled:{[action:string]: any}, test:Tester) {
       return actions.forEach(function(action) {
         test.assertExists(util.action.select(action));
         test.assert(!!enabled[action] == util.action.enabled(action), `Action ${action} should be ${enabled[action] ? "enabled" : "disabled"}`);
       });
     }
   };
+  casper.__util = util;
 }
