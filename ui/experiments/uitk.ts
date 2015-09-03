d272ddbe9284e38f82e87608a2330922abaa9e9a
cmontella <cmontella@live.com>
2015-09-02 18:16:26 -0700

    Add UITK showcase

---
+++ ui/experiments/uitk.ts
@@ -0,0 +1,107 @@
+/// <reference path="../src/microReact.ts" />
+/// <reference path="../src/api.ts" />
+/// <reference path="../src/client.ts" />
+/// <reference path="../src/tableEditor.ts" />
+/// <reference path="../src/glossary.ts" />
+/// <reference path="../src/layout.ts" />
+
+module uitk {
+
+  declare var Papa;
+  declare var uuid;
+  const localState = api.localState;
+  const ixer = api.ixer;
+  const code = api.code;
+  const render = drawn.render;
+
+  function initLocalstate() {}
+
+  function dispatch(event, info, rentrant = false) {
+    var diffs = [];
+    var commands = [];
+    var storeEvent = true;
+
+    switch(event) {   
+      case "":
+
+        break;
+      default:
+        return drawn.dispatch(event, info, rentrant);
+        break;
+    }
+
+    if(!rentrant) {
+      if(diffs.length || commands.length) {
+        let formatted = api.toDiffs(diffs);
+        if(storeEvent && formatted.length) {
+          eveEditor.storeEvent(localState.drawnUiActiveId, event, formatted);
+        }
+        ixer.handleDiffs(formatted);
+        client.sendToServer(formatted, false, commands);
+      }
+      render();
+    }
+    return diffs;
+  }
+
+
+  export function root() {
+    var page:any;
+    return {id: "root", c: localStorage["theme"] || "light", children: [
+      drawn.tooltipUi(),
+      drawn.notice(),
+      {c: "workspace", children: [
+        workspaceTools(),
+        workspaceCanvas(),
+      ]}
+    ]};
+  }
+
+    function workspaceTools() {
+      let actions = {};
+      let disabled = {};
+      return drawn.leftToolbar(actions, disabled);
+    }
+
+  let settingsPanes:ui.Pane[] = [
+    {
+      id: "pane1",
+      title: "Pane 1",
+      content: function() {
+        return {text: "Pane 1"}
+      }
+    },
+    {
+      id: "pane2",
+      title: "Pane 2",
+      content: function() {
+        return {text: "Pane 2"}
+      }
+    },
+    {
+      id: "pane3",
+      title: "Pane 3",
+      content: function() {
+        return {children:[{text: "Pane 3"},ui.button({text: "Button!"})]}
+      }
+    }
+  ];
+
+  function workspaceCanvas() {
+    return {c: "canvas", children: [
+      {text: "This is just some text"},
+      ui.button({text: "Button 1"}),
+      ui.checkbox({change: null}),
+      ui.chart({chartData: ['my data', 30, 200, 100, 400, 150, 250, 30], chartType: "spline"}),
+      ui.tabbedBox({id: "example-pane", semantic: "pane::example", defaultTab: "pane1", panes: settingsPanes, controls: [{c: "ion-close tab", click: null},{c: "ion-search tab", click: null}]}),
+      ui.input({multiline: false}),
+      ui.input({multiline: true}),
+    ]};
+  }
+
+  window["drawn"].root = root;
+
+  client.afterInit(() => {});
+}
+
+
