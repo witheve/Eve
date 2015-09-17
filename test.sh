7a6e998511a624040eafca7dcdc96a010008362e
Joshua Cole <joshuafcole@gmail.com>
2015-09-16 17:11:39 -0700

    Add --error-on-fail flag to spook + better output formatting

--- test.sh
+++ test.sh
@@ -45,42 +45,46 @@ while [[ $# > 0 ]]; do
 done
 
 # Ensure dependencies are installed.
-echo "* Checking dependencies..."
+printf "* Checking dependencies..."
 hash slimerjs 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo "Please install slimerjs with ('npm install -g slimerjs') before continuing."
+  printf "X\n  Please install slimerjs with ('npm install -g slimerjs') before continuing.\n"
   exit 1
 fi
 hash casperjs 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo "Please install casperjs with ('npm install -g casperjs@1.1.0-beta3') before continuing."
+  printf "X\n  Please install casperjs with ('npm install -g casperjs@1.1.0-beta3') before continuing.\n"
   exit 1
 fi
 hash spook 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo "Please install spook with ('npm install -g spook') before continuing."
+  printf "X\n  Please install spook with ('npm install -g spook') before continuing.\n"
   exit 1
 fi
 
 hash gm 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo "Please install GraphicsMagick with ('brew install GraphicsMagick' on mac, or from your dist's repo) before continuing."
+  printf "X\n  Please install GraphicsMagick with ('brew install GraphicsMagick' on mac, or from your dist's repo) before continuing.\n"
   exit 1
 fi
+echo "done."
 
 if [[ $mode == "server" ]]; then
   result="$(spook --server $spookFlags --out "$outDir" --dbd "$taskDir" -- $casperFlags)"
   exit $result
 fi
 
-echo "* Checking if runtime server is up..."
-ps aux | grep '[t]arget/\(debug\|release\)/server'
-if [ $? -ne 0 ]; then
-  echo "Please run the runtime server separately and rerun the test suite after it has fully started."
+printf "* Checking if runtime server is up..."
+ps aux | grep -q '[t]arget/\(debug\|release\)/server'
+res="$?"
+if [ "$res" -ne 0 ]; then
+  printf "X\n  Please run the runtime server separately and rerun the test suite after it has fully started.\n"
   exit 2
 fi
+echo "done."
 
 echo "* Running integration tests..."
 echo "- Output dir is: $outDir"
 
-spook --out "$outDir" --base "ui/bin/test" --tests "$tests" --includes "ui/bin/test/include/**/*.js" $spookFlags -- --verbose --basePath="`pwd`" --engine=slimerjs $casperFlags
+spook --error-on-fail --out "$outDir" --base "ui/bin/test" --tests "$tests" --includes "ui/bin/test/include/**/*.js" $spookFlags -- --verbose --basePath="`pwd`" --engine=slimerjs $casperFlags
+exit $?
