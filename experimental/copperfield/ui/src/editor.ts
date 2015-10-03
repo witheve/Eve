25b930d320a7dd63d1876adeb968ea0deedc9eae
Joshua Cole <joshuafcole@gmail.com>
2015-10-02 18:39:49 -0700

    Realtime edits + dispatchOnEvent DSL for performant inline event handlers

--- experimental/copperfield/ui/src/editor.ts
+++ experimental/copperfield/ui/src/editor.ts
@@ -32,14 +32,61 @@ module Editor {
     }
   }
 
-  var __handlers:{[evt:string]: MicroReact.Handler<Event> } = {};
-  function dispatchOnEvent(evt) {
-    return __handlers[name] || (__handlers[name] = (evt, elem) => dispatch(name, elem).done());
-  }
-
-  var dispatches:{[evt:string]: (info:{}) => DispatchEffect} = {
-    createView: function({}) {
+  var dispatches:{[evt:string]: (info:any) => DispatchEffect} = {
+    parse: function({query}:{query:string}) {
       let effect = DispatchEffect.from(this);
+      localState.ast = localState.reified = localState.msg = undefined;
+      try {
+        localState.ast =  Parsers.query.parse(query);
+        localState.reified = Parsers.query.reify(localState.ast);
+      } catch(err) {
+        if(err.name === "Parse Error") localState.msg = `${err}`;
+        else {
+          console.warn(err.stack);
+          throw err;
+        }
+      }
+
+      effect.rerender = true;
+      return effect;
+    },
+    createViewFromQuery: function({query}:{query:Parsers.ReifiedQuery}) {
+      let effect = DispatchEffect.from(this);
+      let sources = [];
+      for(let source of query.sources) {
+        sources.push({source: source.source, "source view": source.sourceView});
+      }
+      let variables = [];
+      let fields = [];
+      for(let varId in query.variables) {
+        let variable = query.variables[varId];
+        let params = {variable: varId, dependents: <any>{}};
+        variables.push(params);
+        params.dependents.binding = [];
+        for(let binding of variable.bindings) {
+          params.dependents.binding.push({source: binding[0], field: binding[1]});
+        }
+        if(variable.value) params.dependents["constant binding"] = {value: variable.value};
+        if(variable.ordinal) params.dependents["ordinal binding"] = {source: variable.ordinal};
+        if(variable.selected) {
+          fields.push({kind: "output", dependents: {
+            select: {variable: varId}
+          }});
+        }
+      }
+
+      let viewChange = Api.insert("view", {
+        kind: "join",
+        dependents: {
+          source: sources,
+          variable: variables,
+          field: fields
+        }
+      });
+      localState.view = viewChange.context["view"];
+      effect.changes.push(
+        viewChange
+      );
       return effect;
     }
   };
@@ -49,6 +96,33 @@ module Editor {
       return;
     }
     return dispatches[evt].call(this, info);
+  }
+
+
+  var __handlers:{[key:string]: MicroReact.Handler<Event> } = {};
+  function dispatchOnEvent(dispatches:string, commands?:string) {
+    let key = "";
+    if(commands) key += commands;
+    if(dispatches) key += key ? " | " + dispatches : dispatches;
+    if(__handlers[key]) return __handlers[key];
+
+    let code = `
+    var localState = Api.localState;
+    var dispatch = Editor.dispatch;\n`;
+    for(let cmd of commands.split(";")) {
+      code += "    " + cmd.trim() + ";\n";
+    }
+    if(dispatches.length) {
+      let names = dispatches.split(/;,/);
+      let multi = false;
+      for(let name of names) {
+        code += multi ? "\n      ." : "\n    " + `dispatch("${name}", elem)`;
+        multi = true;
+      }
+      code += ".done();\n";
+    }
+
+    return __handlers[key] = <MicroReact.Handler<Event>>new Function("evt", "elem", code);
   }
 
   //---------------------------------------------------------------------------
@@ -80,31 +154,16 @@ module Editor {
   // `;
 
   function root():Element {
-    let parsed;
-    let reified;
-    let msg;
-    try {
-      let ast = Parsers.query.parse(script);
-      parsed = JSON.stringify(ast, null, 2);
-      reified = JSON.stringify(Parsers.query.reify(ast), null, 2);
-    } catch(err) {
-      if(err.name === "Parse Error") msg = `${err}`;
-      else {
-        console.warn(err.stack);
-        throw err;
-      }
-    }
-
     let resultPanes:Ui.Pane[] = [
       {
         title: "AST",
         id: "result-ast",
-        content: {t: "pre", c: "ast", text: parsed}
+        content: {t: "pre", c: "ast", text: JSON.stringify(localState.ast, null, 2)}
       },
       {
         title: "Reified",
         id: "result-reified",
-        content: {t: "pre", c: "reified", text: reified}
+        content: {t: "pre", c: "reified", text: JSON.stringify(localState.reified, null, 2)}
       }
     ];
 
@@ -112,18 +171,14 @@ module Editor {
       {text: "Copperfield"},
       Ui.row({children: [
         Ui.column({flex: 1, children: [
-          Ui.button({text: "compile", click: recompile}),
-          Ui.input({t: "pre", c: "code", flex: 1, text: script}),
-          {t: "pre", c: "err", text: msg}
+          Ui.button({text: "compile", click: dispatchOnEvent("createViewFromQuery", "elem.query = localState.reified")}),
+          Ui.input({t: "pre", c: "code", text: script, input: dispatchOnEvent("parse", "elem.query = evt.target.textContent")}),
+          {t: "pre", c: "err", text: localState.msg},
+          localState.view ? {t: "pre", text: JSON.stringify(Api.ixer.facts(localState.view), null, 2)} : undefined
         ]}),
         Ui.tabbedBox({flex: 1, panes: resultPanes, defaultTab: "result-reified"})
       ]})
     ]};
-  }
-
-  function recompile(evt, elem) {
-    script = document.querySelector(".code").textContent;
-    render();
   }
 
   //---------------------------------------------------------------------------
@@ -132,6 +187,13 @@ module Editor {
   // @FIXME: This should be moved into API once completed.
   interface LocalState {
     initialized: boolean
+
+    activePage?: string
+    activeComponent?: string
+    ast?
+    reified?
+    msg?
+    view?: string
   }
   export var localState:LocalState = {
     initialized: true
@@ -148,6 +210,7 @@ module Editor {
     } else {
       localState = Api.localState;
     }
+    dispatch("parse", {query: script}).done();
     render();
   }
 
