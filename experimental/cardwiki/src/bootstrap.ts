e99dfbb7e49cb3d55eb938ec3a753f7be6464bad
Joshua Cole <joshuafcole@gmail.com>
2015-11-22 04:22:56 -0800

    Move plan parser to parser.ts, add query parser, add addEntity macro

--- experimental/cardwiki/src/bootstrap.ts
+++ experimental/cardwiki/src/bootstrap.ts
@@ -1,285 +1,13 @@
-import {unpad, underline, titlecase} from "./utils"
+import {unpad, titlecase} from "./utils"
 import * as runtime from "./runtime"
 import * as wiki from "./wiki"
 import * as app from "./app"
 import {eve} from "./app"
+import {parsePlan, PlanStep, parseQuery, QueryStep} from "./parser"
 
 export var ixer = eve;
 declare var uuid;
 
-//-----------------------------------------------------------------------------
-// Plan Parser
-//-----------------------------------------------------------------------------
-interface PlanStep {
-  id: string
-  size?: number
-  name?: string
-  type?: string // find | <other>
-  relatedTo?: PlanStep
-}
-interface PlanFind extends PlanStep {
-  entity?: string
-}
-interface PlanGather extends PlanStep {
-  collection?: string
-}
-interface PlanLookup extends PlanStep {
-  attribute: string
-  deselect?: boolean
-}
-interface PlanIntersect extends PlanStep {
-  deselect?: boolean
-  collection: string
-  entity?: string
-}
-interface PlanFilterByEntity extends PlanStep {
-  deselect?: boolean
-  entity: string
-}
-interface PlanFilter extends PlanStep {
-  func: string
-  args: {[param:string]:[string, string]|any} // value may be a [source, alias] pair to be applied or a constant
-}
-interface PlanCalculate extends PlanStep {
-  func: string
-  args: {[param:string]:[string, string]|any} // value may be a [source, alias] pair to be applied or a constant
-}
-interface PlanAggregate extends PlanStep {
-  aggregate: string
-  args: {[param:string]:[string, string]|any} // value may be a [source, alias] pair to be applied or a constant
-}
-interface PlanGroup extends PlanStep {
-  groups: any
-}
-interface PlanSort extends PlanStep {
-  sort: any
-}
-interface PlanLimit extends PlanStep {
-  limit: any
-}
-
-class ParseError extends Error {
-  name: string = "Parse Error";
-
-  constructor(public message:string, public line:string, public lineIx?:number, public charIx:number = 0, public length:number = line.length - charIx) {
-    super(message);
-  }
-  toString() {
-    return unpad(6) `
-      ${this.name}: ${this.message}
-      ${this.lineIx !== undefined ? `On line ${this.lineIx + 1}:${this.charIx}` : ""}
-      ${this.line}
-      ${underline(this.charIx, this.length)}
-    `;
-  }
-}
-
-function readWhile(str:string, substring:string, startIx:number):string {
-  let endIx = startIx;
-  while(str[endIx] === substring) endIx++;
-  return str.slice(startIx, endIx);
-}
-
-function readUntil(str:string, sentinel:string, startIx:number):string;
-function readUntil(str:string, sentinel:string, startIx:number, unsatisfiedErr: Error):string|Error;
-function readUntil(str:string, sentinel:string, startIx:number, unsatisfiedErr?: Error):any {
-  let endIx = str.indexOf(sentinel, startIx);
-  if(endIx === -1) {
-    if(unsatisfiedErr) return unsatisfiedErr;
-    return str.slice(startIx);
-  }
-  return str.slice(startIx, endIx);
-}
-
-export function getAlias(line, lineIx, charIx):[string, number] {
-  let alias = uuid();
-  let aliasIx = line.lastIndexOf("as [");
-  if(aliasIx !== -1) {
-    alias = readUntil(line, "]", aliasIx + 4, new ParseError(`Alias must terminate in a closing ']'`, line, lineIx, line.length));
-    if(alias instanceof Error) return alias;
-  } else aliasIx = undefined;
-  return [alias, aliasIx];
-}
-
-export function getDeselect(line, lineIx, charIx):[boolean, number] {
-  let deselect = false;
-  if(line[charIx] === "!") {
-    deselect = true;
-    charIx++;
-    while(line[charIx] === " ") charIx++;
-  }
-  return [deselect, charIx];
-}
-
-export function getArgs(line, lineIx, charIx):[Error, number]|[{}, number] {
-  let args = {};
-  if(line[charIx] === "{") {
-    let endIx = line.indexOf("}", charIx);
-    if(endIx === -1) return [new ParseError(`Args must terminate in a closing '}'`, line, lineIx, line.length), line.length];
-    for(let pair of line.slice(++charIx, endIx).split(";")) {
-      let [key, val] = pair.split(":");
-      let trimmedVal = val.trim();
-      if(key === undefined || val === undefined)
-        return [new ParseError(`Args must be specified in key: value pairs`, line, lineIx, charIx, pair.length), charIx + pair.length + 1];
-
-      if(trimmedVal[0] === "[") {
-        let valIx = charIx + pair.indexOf("[");
-        if(trimmedVal[trimmedVal.length - 1] !== "]") return [new ParseError(`Attribute Aliases must terminate in a closing ']'`, line, lineIx, valIx), valIx];
-        let [source, attribute] = trimmedVal.slice(1, -1).split(",");
-        if(!attribute) return [new ParseError(`Attribute Aliases must contain a source, attribute pair`, line, lineIx, valIx), valIx];
-        args[key.trim()] = [source.trim(), attribute.trim()];
-      } else {
-        args[key.trim()] = wiki.coerceInput(trimmedVal);
-      }
-
-      charIx += pair.length + 1;
-    }
-    return [args, endIx + 1];
-  }
-  return [new ParseError(`Arguments are specified as {key: value, key2: 7}`, line, lineIx, charIx), charIx];
-}
-
-let parseStep:{[step: string]: (line: string, lineIx: number, charIx: number, related?: PlanStep) => Error|PlanStep} = {
-  // Sources
-  find(line, lineIx, charIx) {
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let entity = line.slice(charIx, aliasIx).trim();
-    if(!entity)
-      return new ParseError(`Find step must specify a valid entity id`, line, lineIx, charIx);
-    let step:PlanFind = {type: "find", id: alias, entity};
-    return step;
-  },
-  gather(line, lineIx, charIx, relatedTo) {
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let collection = line.slice(charIx, aliasIx).trim();
-    if(!collection)
-      return new ParseError(`Gather step must specify a valid collection id`, line, lineIx, charIx);
-    let step:PlanGather = {type: "gather", id: alias, collection, relatedTo};
-    return step;
-  },
-
-  // Joins
-  lookup(line, lineIx, charIx, relatedTo) {
-    if(!relatedTo) return new ParseError(`Lookup step must be a child of a root`, line, lineIx, charIx);
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let deselect;
-    [deselect, charIx] = getDeselect(line, lineIx, charIx);
-    let attribute = line.slice(charIx, aliasIx).trim();
-    if(!attribute)
-      return new ParseError(`Lookup step must specify a valid attribute id.`, line, lineIx, charIx);
-    let step:PlanLookup = {type: "lookup", id: alias, attribute, deselect, relatedTo};
-    return step;
-  },
-  intersect(line, lineIx, charIx, relatedTo) {
-    if(!relatedTo) return new ParseError(`Lookup step must be a child of a root`, line, lineIx, charIx);
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let deselect;
-    [deselect, charIx] = getDeselect(line, lineIx, charIx);
-    let collection = line.slice(charIx, aliasIx).trim();
-
-    if(!collection)
-      return new ParseError(`Intersect step must specify a valid collection id`, line, lineIx, charIx);
-    let step:PlanIntersect = {type: "intersect", id: alias, collection, deselect, relatedTo};
-    return step;
-  },
-  filterByEntity(line, lineIx, charIx, relatedTo) {
-    if(!relatedTo) return new ParseError(`Lookup step must be a child of a root`, line, lineIx, charIx);
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let deselect;
-    [deselect, charIx] = getDeselect(line, lineIx, charIx);
-    let entity = line.slice(charIx, aliasIx).trim();
-    if(!entity)
-      return new ParseError(`Intersect step must specify a valid entity id`, line, lineIx, charIx, entity.length);
-    let step:PlanFilterByEntity = {type: "filter by entity", id: alias, entity, deselect, relatedTo};
-    return step;
-  },
-
-  // Calculations
-  filter(line, lineIx, charIx) {
-    // filter positive
-    // filter >; a: 7, b: [person age]
-    while(line[charIx] === " ") charIx++;
-    let [alias, aliasIx] = getAlias(line, lineIx, charIx);
-    let lastIx = charIx;
-    let filter = readUntil(line, "{", charIx); // @NOTE: Need to remove alias
-    charIx += filter.length;
-    filter = filter.trim();
-    if(!filter)
-      return new ParseError(`Filter step must specify a valid filter id`, line, lineIx, lastIx);
-
-    let args;
-    [args, charIx] = getArgs(line, lineIx, charIx);
-    if(args instanceof Error) return args;
-    if(line.length > charIx) return new ParseError(`Filter step contains extraneous text`, line, lineIx, charIx);
-
-    let step:PlanFilter = {type: "filter", id: alias, func: filter, args};
-    return step;
-  }
-};
-
-export function parsePlan(str:string):PlanStep[] {
-  let plan:PlanStep[] = [];
-  let errors = [];
-  let lineIx = 0;
-  let lines = str.split("\n")
-  let stack:{indent: number, step: PlanStep}[] = [];
-  for(let line of lines) {
-    let charIx = 0;
-    while(line[charIx] === " ") charIx++;
-    let indent = charIx;
-    if(line[charIx] === undefined)  continue;
-    let related;
-    for(let stackIx = stack.length - 1; stackIx >= 0; stackIx--) {
-      if(indent > stack[stackIx].indent) {
-        related = stack[stackIx].step;
-        break;
-      } else stack.pop();
-    }
-    let keyword = readUntil(line, " ", charIx);
-    charIx += keyword.length;
-    let step:Error|PlanStep;
-    if(parseStep[keyword]) step = parseStep[keyword](line, lineIx, charIx, related);
-    else step = new ParseError(`Keyword '${keyword}' is not a valid plan step, ignoring`, line, lineIx, charIx, keyword.length);
-
-    if(step["args"]) {
-      let args = step["args"];
-      for(let arg in args) {
-        if(args[arg] instanceof Array) {
-          let [source] = args[arg];
-          let valid = false;
-          for(let step of plan) {
-            if(step.id === source) {
-              valid = true;
-              break;
-            }
-          }
-          if(!valid) {
-            step = new ParseError(`Alias source '${source}' does not exist in plan`, line, lineIx, charIx)
-          }
-        }
-      }
-    }
-
-    if(step instanceof Error) errors.push(step);
-    else {
-      plan.push(<PlanStep>step);
-      stack.push({indent, step: <PlanStep>step});
-    }
-
-    lineIx++;
-  }
-  if(errors.length) {
-    for(let err of errors) {
-      console.error(err);
-    }
-  }
-  return plan;
-}
 
 //-----------------------------------------------------------------------------
 // Utilities
@@ -287,11 +15,28 @@ export function parsePlan(str:string):PlanStep[] {
 
 function queryFromSearch(search:string):runtime.Query {
   let result = wiki.newSearch(search);
+  result.query.ordinal()
   return result.query;
 }
 export function queryFromPlanDSL(str:string):runtime.Query {
-  let plan:PlanStep[] = parsePlan(str);
-  return wiki.planToQuery(plan);
+  return wiki.planToQuery(parsePlan(str));
+}
+export function queryFromQueryDSL(ixer:runtime.Indexer, str:string):runtime.Query {
+  let plan = parseQuery(str);
+  let query = new runtime.Query(ixer);
+  for(let step of plan) {
+    if(step.type === "select") query.select(step["view"], step["join"] || {}, step.id);
+    else if(step.type === "deselect") query.deselect(step["view"], step["join"] || {});
+    else if(step.type === "calculate") query.calculate(step["func"], step["args"], step.id);
+    else if(step.type === "aggregate") query.aggregate(step["func"], step["args"], step.id);
+    else if(step.type === "ordinal") query.ordinal();
+    else if(step.type === "group") query.group(step["groups"]);
+    else if(step.type === "sort") query.sort(step["sorts"]);
+    else if(step.type === "limit") query.limit(step["limit"]);
+    else if(step.type === "project") query.project(step["mapping"]);
+    else throw new Error(`Unknown query step type '${step.type}'`);
+  }
+  return query;
 }
 
 class BSPhase {
@@ -321,19 +66,29 @@ class BSPhase {
     this.changeset.add(table, fact);
     return this;
   }
+
+  addEntity(entity:string, name:string, kinds:string[], attributes?:{}, extraContent?:string) {
+    let content = unpad(6) `
+      # ${titlecase(name)} (${kinds.map((kind) => `{is a: ${kind}}`).join(", ")})
+    `;
+    if(attributes) {
+      content += "## Attributes\n";
+      for(let attr in attributes) content += `${attr}: {${attr}: ${attributes[attr]}}\n      `;
+    }
+    if(extraContent) content += "\n" + extraContent;
+    this.addFact("builtin entity", {entity, content});
+    return this;
+  }
+
   addView(view:string, kind:string, fields:string[]) {
     this._views[view] = kind;
     this._viewFields[view] = fields;
     this.addFact("view", {view, kind: kind});
     for(let field of fields) this.addFact("field", {view, field});
-    //this.addFact("builtin entity collection", {entity: view, collection: kind});
-    this.addFact("builtin entity", {entity: view, content: unpad(6) `
-      # ${view} ({is a: system} {is a: ${kind}})
-      ${titlecase(view)} is a system view.
-
+    this.addEntity(view, view, ["system", kind], undefined, unpad(6) `
       ## Fields
       ${fields.map((field) => `* ${field}`).join("\n      ")}
-    `});
+    `);
     return this;
   }
 
@@ -354,14 +109,8 @@ class BSPhase {
     // apply the natural mapping.
     if(!mapping) {
       if(this.viewKind(union) !== "union") throw new Error(`Union '${union}' must be added before adding members`);
-      let memberFields = this.viewFields(member);
-      if(!memberFields) throw new Error(`Member '${member}' must be added before adding as a member`);
       mapping = {};
-      for(let field of this.viewFields(union)) {
-        if(memberFields.indexOf(field) === -1)
-          throw new Error(`Cannot apply natural mapping for '${union}' <-- '${member}', member missing field '${field}'`);
-        mapping[field] = field;
-      }
+      for(let field of this.viewFields(union)) mapping[field] = field;
     }
     let action = `${union} <-- ${member}`;
     this.addFact("action", {view: union, action, kind: "union", ix: 0})
@@ -382,19 +131,24 @@ class BSPhase {
 
 app.init("bootstrap", function bootstrap() {
   let phase = new BSPhase(eve);
-  phase.addUnion("entity", ["entity", "content"]);
-  phase.addFact("builtin entity", {entity: "system", content: unpad(4) `
-    # System ({is a: collection})
-  `});
-  phase.addFact("builtin entity", {entity: "union", content: unpad(4) `
-    # Union ({is a: system} {is a: collection})
-  `});
-  phase.addFact("builtin entity", {entity: "query", content: unpad(4) `
-    # Query ({is a: system} {is a: collection})
-  `});
-  phase.addFact("builtin entity", {entity: "table", content: unpad(4) `
-    # Table ({is a: system} {is a: collection})
-  `});
+  phase.addUnion("entity", ["entity", "content"])
+    .addUnionMember("entity", "manual entity")
+    .addUnionMember("entity", "action entity")
+    .addUnionMember("entity", "unmodified added bits")
+    .addUnionMember("entity", "automatic collection entities");
+
+  phase.addEntity("collection", "collection", ["system"])
+    .addEntity("system", "system", ["collection"])
+    .addEntity("union", "union", ["system", "collection"])
+    .addEntity("query", "query", ["system", "collection"])
+    .addEntity("table", "table", ["system", "collection"]);
+
+  phase.addQuery("unmodified added bits", queryFromQueryDSL(phase.ixer, unpad(4) `
+    select added bits as [added]
+    deselect manual entity {entity: [added, entity]}
+    project {entity: [added, entity]; content: [added, content]}
+  `));
+
   phase.apply();
 
   //-----------------------------------------------------------------------------
@@ -424,17 +178,8 @@ app.init("bootstrap", function bootstrap() {
     koala: {length: 3},
     sloth: {length: 3}
   };
-  for(let entity in testData) {
-    let attrs = "";
-    for(let attr in testAttrs[entity] || {}) {
-      attrs += `${attr}: {${attr}: ${testAttrs[entity][attr]}}\n      `;
-    }
-    phase.addFact("builtin entity", {entity, content: unpad(6) `
-      # ${entity} ${entity !== "test data" ? `({is a: test data})` : ""}
-      ${entity} is a ${testData[entity].map((kind) => `{is a: ${kind}}`).join(", ")}.
-      ${attrs}
-    `});
-  }
+  for(let entity in testData) phase.addEntity(entity, entity, ["test data"].concat(testData[entity]), testAttrs[entity]);
+
   phase.addQuery("exotic pet", queryFromPlanDSL(unpad(4) `
     gather pet as [animal]
       intersect exotic
@@ -442,7 +187,7 @@ app.init("bootstrap", function bootstrap() {
       filterByEntity ! snake
       filter > { a: [animal length, value]; b: 1 }
   `));
-  phase.apply();
+  //phase.apply();
   window["p"] = phase;
 });
 
