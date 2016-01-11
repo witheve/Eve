3d8d9558dd26577f5bf5a8dceb7a6c72a407cda1
Joshua Cole <joshuafcole@gmail.com>
2016-01-11 15:14:23 -0800

    package.json and tsd.json cleanup

--- experimental/cardwiki/typings/tsd.d.ts
+++ experimental/cardwiki/typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="express/express.d.ts" />
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
