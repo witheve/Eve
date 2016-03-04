220bb74256f21fff04c697c0581fcd4ab3c877aa
cmontella <cmontella@live.com>
2016-03-04 11:28:42 -0800

    Fleshed out repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,10 +1,12 @@
-import * as parser from "./parser";
+import app = require("./app");
 
 export interface Query {
   type: string,
   query: string,
   id: string,
 }
+
+app.renderRoots["repl"] = root;
 
 let WebSocket = require('ws');
 var server;
@@ -15,18 +17,27 @@ let ws: WebSocket = new WebSocket("ws://localhost:8080");
 ws.onopen = function(e: Event) {
   console.log("Opening websocket connection.");
   console.log(e);
-  let query: Query = {
-    id: uuid(),
-    type: "query",
-    query: "",
-  }
-  ws.send(JSON.stringify(query));
 }
 
 ws.onmessage = function(message: MessageEvent) {
   console.log(message);
   let parsed = JSON.parse(message.data);
+  console.log("Received message");
   console.log(parsed);
+  // Update the result of the correct repl card
+  let targetCard = replCards.filter((r) => r.id === parsed.id).shift();
+  targetCard.submitted = true;
+  if (targetCard !== undefined) {
+    let foo: any = targetCard.children[1];
+    foo.text = "hello";
+    console.log(foo);
+  }
+  // Create a new card if we submitted the last one
+  if (replCards[replCards.length - 1].submitted) {
+    replCards.push(newReplCard());  
+  }
+  app.dispatch("rerender", {}).commit();
+  console.log(replCards);
 }
 
 ws.onerror = function(e: Event) {
@@ -35,4 +46,57 @@ ws.onerror = function(e: Event) {
 
 ws.onclose = function(c: CloseEvent) {
   console.log("Closing websocket connection.");
+}
+
+function sendQuery(ws: WebSocket, query: Query) {
+  console.log("Sending query:");
+  console.log(query);
+  if (ws.readyState === ws.OPEN) {
+    ws.send(JSON.stringify(query));  
+  }
+}
+
+function newReplCard() {
+  function submitQuery(e) {
+    let textArea = e.srcElement;
+    let replCard = textArea.parentElement;
+    // Submit the query
+    if (e.keyCode === 13 && e.ctrlKey === true) {
+      let queryString = textArea.value;
+      let query: Query = {
+        id: replCard._id,
+        type: "query",
+        query: queryString,
+      }
+      sendQuery(ws, query);
+      replCard.query = query;
+    // Catch tab
+    } else if (e.keyCode === 9) {
+      textArea.value += "\t";
+      e.preventDefault();
+    }
+  }
+  
+  let replCard = {
+    id: uuid(),
+    c: "repl-card",
+    submitted: false,
+    children: [
+      {t: "textarea", c: "", placeholder: "query", keydown: submitQuery},
+      {c: "", text: "result"},
+    ],
+  };
+  return replCard;
+}
+
+// Create an initial repl card
+let replCards = [newReplCard()];
+
+function root() {
+  let replroot = {
+    id: "root",
+    c: "repl-root",
+    children: replCards,
+  }; 
+  return replroot;
 }
