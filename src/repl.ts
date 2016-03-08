26a973f60198aa16b7db12c9518b9083c6c4c584
cmontella <cmontella@live.com>
2016-03-07 17:13:59 -0800

    Add focused attribute

--- src/repl.ts
+++ src/repl.ts
@@ -16,6 +16,7 @@ export interface Query {
 interface ReplCard {
   id: string,
   state: CardState,
+  focused: boolean,
   query: string,
   result: {
     fields: Array<string>,
@@ -53,10 +54,6 @@ ws.onmessage = function(message: MessageEvent) {
       targetCard.result = parsed.message;
     }
   }
-  // Create a new card if we submitted the last one in replCards
-  if (replCards[replCards.length - 1].state !== CardState.NONE) {
-    replCards.push(newReplCard());
-  }
   app.dispatch("rerender", {}).commit();
 }
 
@@ -80,52 +77,63 @@ function newReplCard(): ReplCard {
   let replCard: ReplCard = {
     id: uuid(),
     state: CardState.NONE,
-
+    focused: false,
     query: undefined,
     result: undefined,
   }
   return replCard;
 }
 
-function newReplCardElement(replCard: ReplCard) {
-  
-  function submitQuery(event, elem) {
-    let textArea = event.srcElement;
-    let thisReplCard = textArea.parentElement;
-    let replIDs = replCards.map((r) => r.id);
-    // Submit the query with ctrl + enter
-    if (event.keyCode === 13 && event.ctrlKey === true) {
-      let queryString = textArea.value;
-      let query: Query = {
-        id: thisReplCard._id,
-        type: "query",
-        query: queryString,
-      }
-      sendQuery(ws, query);
-    // Catch tab
-    } else if (event.keyCode === 9) {
-      let start = textArea.selectionStart;
-      let end = textArea.selectionEnd;
-      let value = textArea.value;
-      value = value.substring(0, start) + "  " + value.substring(end);
-      textArea.value = value;
-      textArea.selectionStart = textArea.selectionEnd = start + 2;
-      event.preventDefault();
-    // Catch ctrl + arrow up or page up
-    } else if (event.keyCode === 38 && event.ctrlKey === true || event.keyCode === 33) {
-      // Set the focus to the previous repl card
-      let previousIx = replIDs.indexOf(thisReplCard._id) - 1 >= 0 ? replIDs.indexOf(thisReplCard._id) - 1 : 0;
-      let replCardElements: Array<any> = thisReplCard.parentElement.children;
-      console.log(previousIx);
-    // Catch ctrl + arrow down or page down
-    } else if (event.keyCode === 40 && event.ctrlKey === true || event.keyCode === 34) {
-      // Set the focus to the previous repl card
-      let nextIx = replIDs.indexOf(thisReplCard._id) + 1 <= replIDs.length - 1 ? replIDs.indexOf(thisReplCard._id) + 1 : replIDs.length - 1;
-      let replCardElements: Array<any> = thisReplCard.parentElement.children;
-      console.log(nextIx);
+function queryInputKeydown(event, elem) {
+  let textArea = event.srcElement;
+  let thisReplCard = textArea.parentElement;
+  let replIDs = replCards.map((r) => r.id);
+  let thisReplCardIx = replIDs.indexOf(thisReplCard._id);
+  // Submit the query with ctrl + enter
+  if (event.keyCode === 13 && event.ctrlKey === true) {
+    let queryString = textArea.value;
+    let query: Query = {
+      id: thisReplCard._id,
+      type: "query",
+      query: queryString,
     }
+    sendQuery(ws, query);
+    // Create a new card if we submitted the last one in replCards
+    if (thisReplCardIx === replCards.length - 1) {
+      let nReplCard = newReplCard();
+      replCards.forEach((r) => r.focused = false);
+      nReplCard.focused = true;
+      replCards.push(nReplCard);
+    }
+    app.dispatch("rerender", {}).commit();
+  // Catch tab
+  } else if (event.keyCode === 9) {
+    let start = textArea.selectionStart;
+    let end = textArea.selectionEnd;
+    let value = textArea.value;
+    value = value.substring(0, start) + "  " + value.substring(end);
+    textArea.value = value;
+    textArea.selectionStart = textArea.selectionEnd = start + 2;
+    event.preventDefault();
+  // Catch ctrl + arrow up or page up
+  } else if (event.keyCode === 38 && event.ctrlKey === true || event.keyCode === 33) {
+    // Set the focus to the previous repl card
+    let previousIx = thisReplCardIx - 1 >= 0 ? thisReplCardIx - 1 : 0;
+    replCards.forEach((r) => r.focused = false);
+    replCards[previousIx].focused = true;
+    app.dispatch("rerender", {}).commit();
+  // Catch ctrl + arrow down or page down
+  } else if (event.keyCode === 40 && event.ctrlKey === true || event.keyCode === 34) {
+    // Set the focus to the next repl card
+    let nextIx = thisReplCardIx + 1 <= replIDs.length - 1 ? thisReplCardIx + 1 : replIDs.length - 1;
+    replCards.forEach((r) => r.focused = false);
+    replCards[nextIx].focused = true;
+    app.dispatch("rerender", {}).commit();
   }
-  let queryInput = {t: "textarea", c: "query-input", placeholder: "query", keydown: submitQuery, postRender: autoFocus};
+}
+
+function newReplCardElement(replCard: ReplCard) {
+  let queryInput = {t: "textarea", c: "query-input", placeholder: "query", keydown: queryInputKeydown, postRender: replCard.focused === true ? autoFocus : undefined};
   // Set the css according to the card state
   let resultcss;
   if (replCard.state === CardState.NONE) {
@@ -151,6 +159,6 @@ function root() {
     id: "root",
     c: "repl-root",
     children: replCards.map(newReplCardElement),
-  }; 
+  };
   return replroot;
 }
