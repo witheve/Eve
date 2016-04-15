78128a591fcac09d73f7b37de59858d160204697
Joshua Cole <joshuafcole@gmail.com>
2016-04-15 16:17:55 -0700

    Fix jsclient to only return result deltas

--- server/src/server/jsclient.clj
+++ server/src/server/jsclient.clj
@@ -15,6 +15,7 @@
    [clojure.pprint :refer [pprint]]))
 
 (def clients (atom {}))
+(def server (atom nil))
 
 (def DEBUG true)
 (def bag (atom 10))
@@ -35,10 +36,12 @@
 
 (defn send-result [channel id fields results]
   (let [client (get @clients channel)
+        {inserts 'insert removes 'remove} (group-by #(get %1 0) results)
         message {"type" "result"
                  "id" id
                  "fields" fields
-                 "values" results}]
+                 "insert" (map rest inserts)
+                 "remove" (map rest removes)}]
     (httpserver/send! channel (format-json message))
     (when DEBUG
       (println "<- result" id "to" (:id client) "@" (timestamp))
@@ -62,16 +65,17 @@
 
 (defn start-query [db query id channel]
   (let [fields (or (second query) [])
+        store-width (inc (count fields))
         results (atom ())
         [form fields]  (repl/form-from-smil query)
         prog (compiler/compile-dsl db @bag form)
         handler (fn [tuple]
                   (condp = (exec/rget tuple exec/op-register)
-                    'insert (swap! results conj (vec tuple))
+                    'insert (swap! results conj (vec (take store-width tuple)))
                     'flush (do (send-result channel id fields @results)
                                (reset! results '()))
                     'error (send-error channel id (ex-info "Failure to WEASL" {:data (str tuple)}))))
-        e (exec/open db prog handler)]
+        e (exec/open db prog handler false)]
     (e 'insert)
     (e 'flush)))
 
@@ -131,7 +135,6 @@
                    (assoc response :body (slurp (:body response)))
                    response)]
     (println "Serving" (:uri request))
-    (println response)
     (httpserver/send! channel response)))
 
 (defn async-handler [db content]
@@ -144,8 +147,6 @@
 
 (import '[java.io PushbackReader])
 (require '[clojure.java.io :as io])
-
-(def server (atom nil))
 
 (defn serve [db port]
   (println (str "Serving on localhost:" port "/repl"))
