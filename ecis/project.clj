5f873b556e7f29aaec2cc2b6a53837f32e0bceb2
Eric Hoffman <eric@kodowa.com>
2016-04-20 14:24:08 -0700

    .

--- ecis/project.clj
+++ ecis/project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
                  [tentacles "0.5.1"]
+                 [clj-jgit "0.8.9"]
                  [ring/ring-core "1.4.0"]
                  [clj-time "0.11.0"]
                  [org.clojure/data.codec "0.1.0"]
