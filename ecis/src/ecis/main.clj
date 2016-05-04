5dd92d4d5a71463f31218045e6dec17cd7f348b1
eric hoffman <eric@kodowa.com>
2016-05-04 17:27:14 +0000

    .

--- ecis/src/ecis/main.clj
+++ ecis/src/ecis/main.clj
@@ -108,16 +108,16 @@
 (defn eve-insert [s eavs]
   (println "insert" eavs)
   (let [q (str (apply str "(query "
-                      (apply seq (map 
-                                  (fn [t]
-                                    (let [k
-                                          (str "(insert-fact! " 
-                                               "\"" (nth t 0) "\" " 
-                                               (nth t 1) " " 
-                                               "\""(nth t 2) "\")"\n)]
-                                      (println k)
-                                      k))
-                                  eavs))) ")")]
+                      (seq (map 
+                            (fn [t]
+                              (let [k
+                                    (str "(insert-fact! " 
+                                         "\"" (nth t 0) "\" " 
+                                         (nth t 1) " " 
+                                         "\""(nth t 2) "\")"\n)]
+                                (println k)
+                                k))
+                            eavs))) ")")]
     (println "sending insert" q)
     (eve-close (eve-query s q (fn [x] ())))))
 
