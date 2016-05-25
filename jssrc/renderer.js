6742bda0d542617c346b017f701fd7f5ae9bc7f4
Chris Granger <ibdknox@gmail.com>
2016-05-24 17:17:33 -0700

    add in jssrc
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ jssrc/renderer.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function denormalizeResult(result) {
+  let {insert, remove} = result;
+  let elements = {};
+  for(let ins of insert) {
+    let [entity, attribute, value] = ins;
+  }
+  for(let rem of remove) {
+    let [entity, attribute, value] = rem;
+    
+  }
+}
