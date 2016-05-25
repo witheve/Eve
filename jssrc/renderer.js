42c4d8da084130f5c5d4624f3bf0461fe16a04a1
Chris Granger <ibdknox@gmail.com>
2016-05-24 17:27:38 -0700

    add scaffolding to renderer
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- jssrc/renderer.js
+++ jssrc/renderer.js
@@ -2,12 +2,45 @@
 
 function denormalizeResult(result) {
   let {insert, remove} = result;
-  let elements = {};
+  let additions = {};
+  // build up a representation of the additions
   for(let ins of insert) {
     let [entity, attribute, value] = ins;
+    if(!additions[entity]) additions[entity] = {}
+    switch(attribute) {
+      case "tag":
+        break;
+      case "children":
+        break;
+      case "text":
+        attribute = "textContent"
+        break;
+      case "parent":
+        break;
+    }
+    additions[entity][attribute] = value
   }
+  // do removes that aren't just going to be overwritten by
+  // the adds
   for(let rem of remove) {
     let [entity, attribute, value] = rem;
-    
+    switch(attribute) {
+      case "children":
+        break;
+      case "text":
+        attribute = "textContent"
+        break;
+      case "parent":
+        break;
+    }
   }
+
+  let entities = Object.keys(changes)
+  for(let ent of entities) {
+    let attributes = Object.keys(ent);
+    for(let attr of attributes) {
+
+    }
+  }
+
 }
