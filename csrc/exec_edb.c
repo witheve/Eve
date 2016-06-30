7fa8d8f1a91d99df1c12d89f664a8f3037b78dfe
Eric Hoffman <eric@kodowa.com>
2016-06-30 13:55:21 -0700

    some structural changes to line up with the functinos branch, break apart exec.c

---
+++ csrc/exec_edb.c
@@ -0,0 +1,194 @@
+#include <runtime.h>
+#include <exec.h>
+
+// break this out...also copy r now that its well-formed how to do that
+static CONTINUATION_6_4(scan_listener_3, execf, operator, value *, int, int, int,
+                        value, value, value, eboolean);
+static void scan_listener_3(execf n,  operator op, value *r, int a, int b, int c,
+                            value av, value bv, value cv, eboolean present)
+{
+    r[a] = av;
+    r[b] = bv;
+    r[c] = cv;
+    apply(n, 0, r);
+}
+
+static CONTINUATION_5_3(scan_listener_2, execf, operator, value *, int, int, value, value, eboolean);
+static void scan_listener_2(execf n, operator op, value *r, int a, int b,
+                            value av, value bv, eboolean present)
+{
+    r[a] = av;
+    r[b] = bv;
+    apply(n, 0, r);
+}
+
+static CONTINUATION_4_2(scan_listener_1, execf, operator, value *, int, value, eboolean);
+static void scan_listener_1(execf n, operator op, value *r, int a, value av, eboolean present)
+{
+    r[a] = av;
+    apply(n, op, r);
+}
+
+static CONTINUATION_3_1(scan_listener_0, execf, operator, value *, eboolean);
+static void scan_listener_0(execf n, operator op, value *r, eboolean present)
+{
+    apply(n, op, r);
+}
+
+
+static CONTINUATION_7_2(do_scan, evaluation, int *, execf, int, value, value, value, operator, value *);
+static void do_scan(evaluation ex, int *count, execf n, int sig, value e, value a, value v, operator op, value *r)
+{
+    if (op == op_flush) {
+        apply(n, op, r);
+        return;
+    }
+
+    void *listen;
+
+    *count = *count + 1;
+    // generify this too
+    switch(sig) {
+    case s_eav:
+        listen = cont(ex->h, scan_listener_3, n, op, r, reg(e), reg(a), reg(v));
+        break;
+    case s_eAv:
+        listen = cont(ex->h, scan_listener_2, n, op, r, reg(e), reg(v));
+        break;
+    case s_eAV:
+        listen = cont(ex->h, scan_listener_1, n, op, r, reg(e));
+        break;
+    case s_Eav:
+        listen = cont(ex->h, scan_listener_2, n, op, r, reg(a), reg(v));
+        break;
+    case s_EAv:
+        listen = cont(ex->h, scan_listener_1, n, op, r, reg(v));
+        break;
+    case s_EAV:
+        listen = cont(ex->h, scan_listener_0, n, op, r);
+        break;
+    default:
+        exec_error(ex, "unknown scan");
+    }
+
+    apply(ex->s, sig, listen, lookup(e, r), lookup(a, r), lookup(v, r));
+}
+
+static inline boolean is_cap(unsigned char x) {return (x >= 'A') && (x <= 'Z');}
+
+static execf build_scan(evaluation ex, node n)
+{
+    vector ar = vector_get(n->arguments, 0);
+    estring description = vector_get(ar, 0);
+    int sig = 0;
+    for (int i=0; i< 3; i++) {
+        sig <<= 1;
+        sig |= is_cap(description->body[i]);
+    }
+    return cont(ex->h, do_scan, ex,
+                register_counter(ex, n),
+                resolve_cfg(ex, n, 0),
+                sig,
+                vector_get(ar, 1),
+                vector_get(ar, 2),
+                vector_get(ar, 3));
+
+}
+
+static CONTINUATION_7_2(do_insert, evaluation, int *, execf, value, value, value, value, operator, value *) ;
+static void do_insert(evaluation ex, int *count, execf n, value uuid, value e, value a, value v, operator op, value *r)
+{
+    if (op == op_insert) {
+        *count = *count + 1;
+        apply(ex->insert, uuid, lookup(e, r), lookup(a, r), lookup(v, r));
+    }
+    if (op == op_remove) {
+        apply(ex->remove, uuid, lookup(e, r), lookup(a, r), lookup(v, r));
+    }
+    apply(n, op, r);
+}
+
+static execf build_insert(evaluation e, node n)
+{
+    vector a = vector_get(n->arguments, 0);
+    bag x = table_find(e->scopes, vector_get(a, 0));
+    return cont(e->h, do_insert,  e, register_counter(e, n),
+                resolve_cfg(e, n, 0),
+                edb_uuid(x),
+                vector_get(a, 1),
+                vector_get(a, 2),
+                vector_get(a, 3));
+}
+
+static CONTINUATION_7_2(do_remove, evaluation, int *, execf, value, value, value, value, operator, value *) ;
+static void do_remove(evaluation ex, int *count, execf n, value uuid, value e, value a, value v, operator op, value *r)
+{
+    *count = *count + 1;
+    apply(ex->remove, uuid, lookup(e, r), lookup(a, r), lookup(v, r));
+    apply(n, op, r);
+}
+
+static execf build_remove(evaluation e, node n)
+{
+    vector a = vector_get(n->arguments, 0);
+    bag x = table_find(e->scopes, vector_get(a, 0));
+    return cont(e->h, do_remove,  e, register_counter(e, n),
+                resolve_cfg(e, n, 0),
+                edb_uuid(x),
+                vector_get(a, 1),
+                vector_get(a, 2),
+                vector_get(a, 3));
+}
+
+static CONTINUATION_7_2(do_set, evaluation, int *, execf, value, value, value, value, operator, value *) ;
+static void do_set(evaluation ex, int *count, execf n, value uuid, value e, value a, value v, operator op, value *r)
+{
+    *count = *count + 1;
+    apply(ex->set, uuid, lookup(e, r), lookup(a, r), lookup(v, r));
+    apply(n, op, r);
+}
+
+static execf build_set(evaluation e, node n)
+{
+    vector a = vector_get(n->arguments, 0);
+    bag x = table_find(e->scopes, vector_get(a, 0));
+    return cont(e->h, do_set,  e, register_counter(e, n),
+                resolve_cfg(e, n, 0),
+                edb_uuid(x),
+                vector_get(n->arguments, 1),
+                vector_get(n->arguments, 2),
+                vector_get(n->arguments, 3));
+}
+
+
+static CONTINUATION_4_2(do_genid, evaluation, int *, execf, value,  operator, value *);
+static void do_genid(evaluation ex, int *count, execf n, value dest, operator op, value *r)
+{
+    if (op != op_flush) {
+        *count = *count+1;
+        value v = generate_uuid();
+        r[reg(dest)] = v;
+    }
+    apply(n, op, r);
+}
+
+
+static execf build_genid(evaluation e, node n)
+{
+    return cont(e->h, do_genid,
+                e,
+                register_counter(e, n),
+                resolve_cfg(e, n, 0),
+                vector_get(vector_get(n->arguments, 0), 0));
+}
+
+extern void register_edb_builders(table builders)
+{
+    table_set(builders, intern_cstring("insert"), build_insert);
+    table_set(builders, intern_cstring("remove"), build_remove);
+    table_set(builders, intern_cstring("set"), build_set);
+    table_set(builders, intern_cstring("scan"), build_scan);
+    table_set(builders, intern_cstring("generate"), build_genid);
+}
+
+
---
+++ csrc/exec_expression.c
@@ -0,0 +1,247 @@
+#include <runtime.h>
+#include <math.h>
+#include <exec.h>
+
+
+
+static CONTINUATION_5_2(do_equal, evaluation, int *, execf, value, value,  operator, value *); \
+static void do_equal(evaluation e, int *count, execf n, value a, value b, operator op, value *r) 
+{
+    *count = *count + 1;                        
+    if (op != op_flush) {
+        value ar = lookup(a, r);                    
+        value br = lookup(b, r);                
+        if (!value_equals(ar, br)) return;
+    }
+    apply(n, op, r);
+}
+
+
+#define DO_UNARY_NUMERIC(__name, __op)                                                               \
+    static CONTINUATION_5_2(__name, evaluation, int *, execf, value, value, operator, value *);      \
+    static void __name (evaluation ex, int *count, execf n, value dest, value a, operator op, value *r) \
+    {                                                                                                \
+        if (op == op_flush)  {                                                                       \
+            apply(n, op, r);                                                                         \
+            return;                                                                                  \
+        }                                                                                            \
+        value ar = lookup(a, r);                                                                     \
+        *count = *count + 1;                                                                         \
+        if ((type_of(ar) != float_space )) {                                                         \
+            exec_error(ex, "attempt to do math on non-number", a);                                   \
+        } else {                                                                                     \
+            r[reg(dest)] = box_float(__op(*(double *)ar));                                           \
+            apply(n, op, r);                                                                         \
+        }                                                                                            \
+    }
+
+#define DO_UNARY_BOOLEAN(__name, __op)                                                               \
+    static CONTINUATION_5_2(__name, evaluation, int *, execf, value, value, operator, value *);      \
+    static void __name (evaluation ex, int *count, execf n, value dest, value a, operator op, value *r) \
+    {                                                                                                \
+        if (op == op_flush)  {                                                                       \
+            apply(n, op, r);                                                                         \
+            return;                                                                                  \
+        }                                                                                            \
+        value ar = lookup(a, r);                                                                     \
+        *count = *count + 1;                                                                         \
+        if ((type_of(ar) != float_space )) {                                                         \
+            exec_error(ex, "attempt to do math on non-number", a);                                   \
+        } else {                                                                                     \
+          r[reg(dest)] = __op(*ar == etrue ? true : false) ? etrue : efalse;                         \
+            apply(n, op, r);                                                                         \
+        }                                                                                            \
+    }
+
+#define BUILD_UNARY(__name, __do_op)   \
+    static execf __name (evaluation e, node n)  \
+    {                                           \
+        return cont(e->h,                       \
+                __do_op,                        \
+                e,                              \
+                register_counter(e, n),         \
+                resolve_cfg(e, n, 0),           \
+                vector_get(n->arguments, 0),    \
+                vector_get(n->arguments, 1));   \
+    }
+
+
+#define DO_BINARY_NUMERIC(__name, __op)                                                              \
+    static CONTINUATION_6_2(__name, evaluation, int *, execf, value, value, value,  operator, value *);\
+    static void __name (evaluation ex, int *count, execf n, value dest, value a, value b, operator op, value *r) \
+    {                                                                                                \
+        if (op == op_flush)  {                                                                       \
+            apply(n, op, r);                                                                         \
+            return;                                                                                  \
+        }       \
+        value ar = lookup(a, r);                                                                     \
+        value br = lookup(b, r);                                                                     \
+        *count = *count + 1;                                                                         \
+        if ((type_of(ar) != float_space ) || (type_of(br) != float_space)) {                         \
+            exec_error(ex, "attempt to " #__name" non-numbers", a, b);                               \
+            prf("UHOH %v, %v\n", ar, br);                                                            \
+        } else {                                                                                     \
+            r[reg(dest)] = box_float(*(double *)ar __op *(double *)br);                              \
+            apply(n, op, r);                                                                         \
+        }                                                                                            \
+    }
+
+#define DO_BINARY_BOOLEAN(__name, __op)                                                                \
+    static CONTINUATION_6_2(__name, evaluation, int *, execf, value, value, value,  operator, value *);\
+    static void __name (evaluation ex, int *count, execf n, value dest, value a, value b, operator op, value *r) \
+    {                                                                                                  \
+        if (op == op_flush)  {                                                                       \
+            apply(n, op, r);                                                                         \
+            return;                                                                                  \
+        }                                                                                            \
+        value ar = lookup(a, r);                                                                       \
+        value br = lookup(b, r);                                                                       \
+        *count = *count + 1;                                                                           \
+        if ((type_of(ar) != float_space ) || (type_of(br) != float_space)) {                           \
+            exec_error(ex, "attempt to __op non-numbers", a, b);                                       \
+        } else {                                                                                       \
+          r[reg(dest)] = (*(double *)ar __op *(double *)br) ? etrue : efalse;                          \
+            apply(n, op, r);                                                                           \
+        }                                                                                              \
+    }
+
+
+#define BUILD_BINARY(__name, __do_op)   \
+    static execf __name (evaluation e, node n)  \
+    {                                           \
+        vector a = vector_get(n->arguments, 0); \
+        return cont(e->h,                       \
+                __do_op,                        \
+                e,                              \
+                register_counter(e, n),         \
+                resolve_cfg(e, n, 0),           \
+                vector_get(a, 0),    \
+                vector_get(a, 1),    \
+                vector_get(a, 2));   \
+    }
+
+
+#define DO_BINARY_FILTER(__name, __op)                                                               \
+    static CONTINUATION_5_2(__name, evaluation, int *, execf, value, value,  operator, value *);     \
+    static void __name (evaluation ex, int *count, execf n, value a, value b, operator op, value *r) \
+    {                                                                                                \
+        if (op == op_flush)  {                                                                       \
+            apply(n, op, r);                                                                         \
+        }                                                                                            \
+        value ar = lookup(a, r);                                                                     \
+        value br = lookup(b, r);                                                                     \
+        *count = *count + 1;                                                                         \
+        if ((type_of(ar) == float_space ) && (type_of(br) == float_space)) {                         \
+            if (*(double *)ar __op *(double *)br)                                                    \
+            {                                                                                        \
+                apply(n, op, r);                                                                     \
+            }                                                                                        \
+        }                                                                                            \
+        else                                                                                         \
+          {                                                                                          \
+            exec_error(ex, "@FIXME filter non-numbers", a, b);                                       \
+          }                                                                                          \
+    }
+
+
+#define BUILD_BINARY_FILTER(__name, __do_op)   \
+    static execf __name (evaluation e, node n)  \
+    {                                           \
+        vector a = vector_get(n->arguments, 0); \
+        return cont(e->h,                       \
+                __do_op,                        \
+                e,                              \
+                register_counter(e, n),         \
+                resolve_cfg(e, n, 0),           \
+                vector_get(a, 0),    \
+                vector_get(a, 1));   \
+    }
+
+
+DO_UNARY_NUMERIC(do_sin, sin)
+BUILD_UNARY(build_sin, do_sin)
+
+DO_UNARY_NUMERIC(do_cos, cos)
+BUILD_UNARY(build_cos, do_cos)
+
+DO_UNARY_NUMERIC(do_tan, tan)
+BUILD_UNARY(build_tan, do_tan)
+
+DO_BINARY_NUMERIC(do_plus, +)
+BUILD_BINARY(build_plus, do_plus)
+
+DO_BINARY_NUMERIC(do_minus, -)
+BUILD_BINARY(build_minus, do_minus)
+
+DO_BINARY_NUMERIC(do_multiply, *)
+BUILD_BINARY(build_multiply, do_multiply)
+
+DO_BINARY_NUMERIC(do_divide, /)
+BUILD_BINARY(build_divide, do_divide)
+
+DO_BINARY_FILTER(do_less_than, <)
+BUILD_BINARY_FILTER(build_less_than, do_less_than)
+DO_BINARY_BOOLEAN(do_is_less_than, <)
+BUILD_BINARY(build_is_less_than, do_is_less_than)
+
+DO_BINARY_FILTER(do_less_than_or_equal, <=)
+BUILD_BINARY_FILTER(build_less_than_or_equal, do_less_than_or_equal)
+DO_BINARY_BOOLEAN(do_is_less_than_or_equal, <=)
+BUILD_BINARY(build_is_less_than_or_equal, do_is_less_than_or_equal)
+
+DO_BINARY_FILTER(do_greater_than, >)
+BUILD_BINARY_FILTER(build_greater_than, do_greater_than)
+DO_BINARY_BOOLEAN(do_is_greater_than, >)
+BUILD_BINARY(build_is_greater_than, do_is_greater_than)
+
+DO_BINARY_FILTER(do_greater_than_or_equal, >=)
+BUILD_BINARY_FILTER(build_greater_than_or_equal, do_greater_than_or_equal)
+DO_BINARY_BOOLEAN(do_is_greater_than_or_equal, >=)
+BUILD_BINARY(build_is_greater_than_or_equal, do_is_greater_than_or_equal)
+
+// @TODO: make assign do its job instead of just filtering
+//DO_BINARY_FILTER(do_equal, ==)
+BUILD_BINARY_FILTER(build_equal, do_equal)
+DO_BINARY_BOOLEAN(do_is_equal, ==)
+BUILD_BINARY(build_is_equal, do_is_equal)
+
+DO_BINARY_FILTER(do_not_equal, !=)
+BUILD_BINARY_FILTER(build_not_equal, do_not_equal)
+DO_BINARY_BOOLEAN(do_is_not_equal, !=)
+BUILD_BINARY(build_is_not_equal, do_is_not_equal)
+
+static CONTINUATION_5_2(do_is, evaluation, int *, execf, value, value, operator, value *);
+static void do_is (evaluation ex, int *count, execf n, value dest, value a, operator op, value *r)
+{
+  *count = *count + 1;
+  r[reg(dest)] = lookup(a, r);
+  apply(n, op, r);
+}
+
+BUILD_UNARY(build_is, do_is)
+
+
+void register_exec_expression(table builders)
+{
+    table_set(builders, intern_cstring("plus"), build_plus);
+    table_set(builders, intern_cstring("minus"), build_minus);
+    table_set(builders, intern_cstring("multiply"), build_multiply);
+    table_set(builders, intern_cstring("divide"), build_divide);
+    table_set(builders, intern_cstring("less_than"), build_less_than);
+    table_set(builders, intern_cstring("less_than_or_equal"), build_less_than_or_equal);
+    table_set(builders, intern_cstring("greater_than"), build_greater_than);
+    table_set(builders, intern_cstring("greater_than_or_equal"), build_greater_than_or_equal);
+    table_set(builders, intern_cstring("equal"), build_equal);
+    table_set(builders, intern_cstring("not_equal"), build_not_equal);
+    table_set(builders, intern_cstring("is"), build_is);
+    table_set(builders, intern_cstring("is_less_than"), build_is_less_than);
+    table_set(builders, intern_cstring("is_less_than_or_equal"), build_is_less_than_or_equal);
+    table_set(builders, intern_cstring("is_greater_than"), build_is_greater_than);
+    table_set(builders, intern_cstring("is_greater_than_or_equal"), build_is_greater_than_or_equal);
+    table_set(builders, intern_cstring("is_equal"), build_is_equal);
+    table_set(builders, intern_cstring("is_not_equal"), build_is_not_equal);
+    table_set(builders, intern_cstring("sin"), build_sin);
+    table_set(builders, intern_cstring("cos"), build_cos);
+    table_set(builders, intern_cstring("tan"), build_tan);
+}
+
