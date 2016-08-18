66740fcc7074dafd27f390bb739d8f0ada730987
Joshua Cole <joshuafcole@gmail.com>
2016-08-17 18:30:12 -0700

    Add lua Bag which for easy interaction with EAVs

--- src/db.lua
+++ src/db.lua
@@ -249,4 +249,248 @@ function getArgs(schema, bindings)
   return args, fields
 end
 
+function hashFact(fact)
+  local hash = string.format("%s ⦷ %s ⦷ %s", fact[1], fact[2], fact[3]);
+  return hash
+end
+
+local function patternSignature(pattern)
+  local sig = ""
+  if pattern[1] ~= nil then sig[1] = "E" else sig[1] = "e" end
+  if pattern[2] ~= nil then sig[2] = "A" else sig[2] = "a" end
+  if pattern[3] ~= nil then sig[3] = "V" else sig[3] = "v" end
+  return sig
+end
+
+local Bag = {}
+Bag.__index = Bag
+function Bag:new(args)
+  local instance = setmetatable({facts = {}, hashes = Set:new(), indexEAV = {}, indexAVE = {}}, self)
+
+  return instance
+end
+
+function Bag:size()
+  return self.hashes:length()
+end
+
+function Bag:add(fact)
+  local hash = hashFact(fact)
+  if self.hashes:add(hash) then
+    self.facts[hash] = fact
+    self:_indexFact(fact)
+    return true
+  end
+  return false
+end
+
+function Bag:remove(fact)
+  local hash = hashFact(fact)
+  if self.hashes:remove(hash) then
+    self.facts[hash] = nil
+    self:_deindexFact(fact)
+  end
+  return false
+end
+
+function Bag:addMany(facts)
+  local changed = false
+  for _, fact in ipairs(facts) do
+    changed = changed or self:add(fact)
+  end
+  return changed
+end
+
+function Bag:removeMany(facts)
+  local changed = false
+  for _, fact in ipairs(facts) do
+    changed = changed or self:remove(fact)
+  end
+  return changed
+end
+
+function Bag:has(fact)
+  local hash = hashFact(fact)
+  return self.hashes:has(hash)
+end
+
+function Bag:hasAny(facts)
+  for _, fact in ipairs(facts) do
+    if self:has(fact) then
+      return true
+    end
+  end
+  return false
+end
+
+function Bag:hasAll(facts)
+  for _, fact in ipairs(facts) do
+    if not self:has(fact) then
+      return false
+    end
+  end
+  return true
+end
+
+function Bag:union(other)
+  local changed = false
+  for hash, fact in pairs(other.facts) do
+    if not self.hashes:has(hash) then
+      changed = true
+      self.hashes:add(hash)
+      self.facts[hash] = fact
+    end
+  end
+  return changed
+end
+
+function Bag:difference(bag)
+  local changed = false
+  for hash, fact in pairs(other.facts) do
+    if self.hashes:has(hash) then
+      changed = true
+      self.hashes:remove(hash)
+      self.facts[hash] = nil
+    end
+  end
+  return changed
+end
+
+function Bag:_indexFact(fact)
+  local hash = hashFact(fact)
+  local e, a, v = fact[1], fact[2], fact[3]
+
+  if not self.indexEAV[e] then self.indexEAV[e] = {} end
+  if not self.indexEAV[e][a] then self.indexEAV[e][a] = {} end
+  self.indexEAV[e][a][v] = fact
+
+  if not self.indexAVE[a] then self.indexAVE[a] = {} end
+  if not self.indexAVE[a][v] then self.indexAVE[a][v] = {} end
+  self.indexAVE[a][v][e] = fact
+
+  return true
+end
+
+function Bag:_deindexFact(fact)
+  local hash = hashFact(fact)
+  local e, a, v = fact[1], fact[2], fact[3]
+
+  if self.indexEAV[e] and self.indexEAV[e][a] then
+    self.indexEAV[e][a][v] = nil
+  end
+
+  if self.indexAVE[a]and self.indexAVE[a][v] then
+    self.indexAVE[a][v][e] = nil
+  end
+  return true
+end
+
+function Bag:getRecord(entity)
+  local record = {}
+  local eavs = self:find({entity, nil, nil})
+  for eav in pairs(eavs) do
+    record[eav[2]] = eav[3]
+  end
+
+  return record
+end
+
+function Bag:find(pattern)
+  local e, a, v = pattern[1], pattern[2], pattern[3]
+  local sig = patternSignature(pattern)
+  local result = Set:new(0)
+  if sig == "EAV" then
+    local idx = self.indexEAV[e]
+    if idx and idx[a] and idx[a][v] then
+      result:add(idx[a][v])
+    end
+  elseif sig == "EAv" then
+    local idx = self.indexEAV[e]
+    if idx and a and idx[a] then
+      for v, fact in pairs(idx[a]) do
+        result:add(fact)
+      end
+    end
+  elseif sig == "eAV" then
+    local idx = self.indexAVE[a]
+    if idx and idx[v] then
+      for v, fact in pairs(idx[v]) do
+        result:add(fact)
+      end
+    end
+  elseif sig == "Eav" then
+    local idx = self.indexEAV[e]
+    if idx then
+      for a, vtable in pairs(idx) do
+        for v, fact in pairs(vtable) do
+          result:add(fact)
+        end
+      end
+    end
+  elseif sig == "eAv" then
+    local idx = self.indexAVE[a]
+    if idx then
+      for v, etable in pairs(idx) do
+        for e, fact in pairs(etable) do
+          result:add(fact)
+        end
+      end
+    end
+  else
+    for hash, fact in pairs(self.facts) do
+      if v ~= nil or fact[2] == v then
+        result:add(fact)
+      end
+    end
+  end
+
+  return result
+end
+
+function Bag:findRecords(record)
+  local eavs = {}
+  for k, v in pairs(record) do
+    eavs[#eavs + 1] = {nil, k, v}
+  end
+
+  if #eavs < 1 then return Set:new() end
+
+  local entities = Set:new()
+  for fact in pairs(self:find(eavs[#eavs])) do
+    entities:add(fact[1])
+  end
+  eavs[#eavs] = nil
+
+  for eav in ipairs(eavs) do
+    local matchedEntities = Set:new()
+    for fact in pairs(self:find(eav)) do
+      matchedEntities:add(fact[1])
+    end
+    entities:intersection(matchedEntities, true)
+
+    if entities:length() == 0 then
+      return entities
+    end
+  end
+
+  local result = Set:new()
+  for entity in pairs(entities) do
+    result:add(self:getRecord(entity))
+  end
+
+  return result
+end
+
+function Bag:addRecord(record, id)
+  if not id then
+    id = util.generateId() -- @FIXME: This really needs to be a C uuid...
+  end
+  local eavs = {}
+  for k, v in pairs(record) do
+    eavs[#eavs + 1] = {id, k, v}
+  end
+
+  self:addMany(eavs)
+end
+
 return Pkg
