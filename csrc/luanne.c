88752dd527bccaf63588f30e1c4383b8ffc2c54e
Joshua Cole <joshuafcole@gmail.com>
2016-08-24 20:54:42 -0700

    Add lua_stack_dump fn to help debug stack issues

--- csrc/luanne.c
+++ csrc/luanne.c
@@ -2,6 +2,37 @@
 #include <luanne.h>
 #include <http/http.h>
 
+extern string edb_dump(heap h, edb b);
+
+static void lua_stack_dump (lua_State *L) {
+    int i;
+    int top = lua_gettop(L);
+    for (i = 1; i <= top; i++) {  /* repeat for each level */
+        int t = lua_type(L, i);
+        switch (t) {
+
+        case LUA_TSTRING:  /* strings */
+            printf("%d. `%s'", i, lua_tostring(L, i));
+            break;
+
+        case LUA_TBOOLEAN:  /* booleans */
+            printf("%d. %s", i, lua_toboolean(L, i) ? "true" : "false");
+            break;
+
+        case LUA_TNUMBER:  /* numbers */
+            printf("%d. %g", i, lua_tonumber(L, i));
+            break;
+
+        default:  /* other values */
+            printf("%d. %s", i, lua_typename(L, t));
+            break;
+
+        }
+        printf(",  ");  /* put a separator */
+    }
+    printf("\n");  /* end the listing */
+}
+
 static char *luat(lua_State *L, int index)
 {
     return (char *)lua_typename(L, lua_type(L, index));
