153bed5331322409eb5fb8661390837fe07fcd0f
Chris Granger <ibdknox@gmail.com>
2016-10-24 09:00:09 -0700

    add findRecordsFromToken
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/runtime/browser.ts
+++ src/runtime/browser.ts
@@ -169,6 +169,11 @@ class Responder {
       let extraInfo = {};
       let spanId = analyzer.findMaybeDrawers(evaluation, data, spans, extraInfo);
       this.send(JSON.stringify(data));
+    } else if(data.type === "findRecordsFromToken") {
+      let spans = [];
+      let extraInfo = {};
+      let spanId = analyzer.findRecordsFromToken(evaluation, data, spans, extraInfo);
+      this.send(JSON.stringify(data));
     }
   }
 }
@@ -214,6 +219,6 @@ export function init(code) {
   }
 
   client.socket.onopen();
-  // responder.handleEvent(JSON.stringify({type: "findMaybeDrawers", requestId: 0}));
+  // responder.handleEvent(JSON.stringify({type: "findRecordsFromToken", token: ["editor|block|19|token|18"], requestId: 0}));
   // responder.handleEvent(JSON.stringify({type: "findSource", span: "editor|block|18|node|19", requestId: 0}));
 }
