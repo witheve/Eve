bc0e3762e1c6538556dc2cafe733b9f70506ef99
Chris Granger <ibdknox@gmail.com>
2016-10-27 11:16:35 -0700

    send the entire info line
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/runtime/parser.ts
+++ src/runtime/parser.ts
@@ -1403,10 +1403,11 @@ export function parseDoc(doc, docId = `doc|${docIx++}`) {
   let allErrors = [];
   for(let block of blocks) {
     let {results, lex, errors} = parseBlock(block.literal, block.id, block.startOffset, spans, extraInfo);
+    extraInfo[block.id] = {info: block.info};
     // if this block is disabled, we want the parsed spans and such, but we don't want
     // the block to be in the set sent to the builder
     if(block.info.indexOf("disabled") > -1) {
-      extraInfo[block.id] = {disabled: true};
+      extraInfo[block.id].disabled = true;
     } else if(errors.length) {
       allErrors.push(errors);
     } else {
