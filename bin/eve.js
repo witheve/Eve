c24d11406d1dcd3f9dd47f9016c4d57356c49518
Joshua Cole <joshuafcole@gmail.com>
2016-11-28 17:59:45 -0800

    Fix loading quickstart on global install + local control when running examples globally

--- bin/eve.js
+++ bin/eve.js
@@ -38,13 +38,19 @@ var eveRoot = findRoot(__dirname);
 // If we're executing within the eve module/repo, we're running internally and should expose our examples, src, etc.
 // This should be handled down the road by some sort of a manifest in conjunction with the `root` rather than hardcoding.
 if(root === eveRoot) internal = true;
+else if(!root) {
+  internal = true;
+  // We shouldn't (and when globally installed, *can't*) taint the internal examples when running as an installed binary.
+  // @TODO: In the future we should have a more flexible solution that can copy out the examples into your current workspace when edited.
+  controlOwner = Owner.client;
+}
 
 
 // If we're not given an explicit filepath to run, assume the user wanted the editor (rather than a blank page).
 // Similarly, if we're running internally, send the user over to the quickstart, since they're likely testing the waters.
 if(!filepath) {
   editor = true;
-  if(internal) filepath = "examples/quickstart.eve";
+  if(internal) filepath = eveRoot + "/" + "examples/quickstart.eve";
 } else {
   filepath = path.resolve(filepath);
 }
