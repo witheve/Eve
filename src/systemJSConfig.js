ac02abb40b7c1c83ec2f15343534d9288e92d365
Joshua Cole <joshuafcole@gmail.com>
2017-02-10 15:36:29 -0800

    Update SystemJSConfig to support serving from node_modules

--- src/systemJSConfig.js
+++ src/systemJSConfig.js
@@ -1,18 +1,13 @@
 SystemJS.config({
-  baseURL: "build/",
-  defaultJSExtensions: true,
-  map: {fs: "@empty", path: "@empty", glob: "@empty", mkdirp: "@empty"},
-  meta: {"/build/src/codemirror.js": { format: "cjs" }}
-});
-
-// Kicks off requiring everyone else
-SystemJS.register("uuid", [], function($export) {
-  $export("default", uuid);
-  $export("v4", uuid);
-});
-SystemJS.registerDynamic("codemirror", [], false, function(require, exports, module) {
-  module.exports = window.CodeMirror;
-});
-SystemJS.register("microReact", [], function($export) {
-  $export("Renderer", Renderer);
+  baseURL: "node_modules/",
+  map: {
+    fs: "@empty",
+    path: "@empty",
+    glob: "@empty",
+    mkdirp: "@empty"
+  },
+  packages: {
+    "/build": {defaultExtension: "js"},
+    "node-uuid": {main: "uuid.js"}
+  }
 });
