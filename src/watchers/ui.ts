0b516859192bd34a21da7154ff47a9da056aa7a6
Joshua Cole <joshuafcole@gmail.com>
2017-02-10 19:35:49 -0800

    Add more helpers + button blocks

--- src/watchers/ui.ts
+++ src/watchers/ui.ts
@@ -1,11 +1,47 @@
 import {Watcher, RawMap, RawValue, RawEAV} from "./watcher";
 import {v4 as uuid} from "node-uuid";
 
-interface Attrs extends RawMap<RawValue> {}
+interface Attrs extends RawMap<RawValue|RawValue[]|RawEAV[]> {}
 
 export class UIWatcher extends Watcher {
+  protected static _addAttrs(id:string, attrs?: Attrs, eavs:RawEAV[] = []) {
+    if(attrs) {
+      for(let attr in attrs) {
+        if(attrs[attr].constructor !== Array) {
+          eavs.push([id, attr, attrs[attr] as RawValue]);
 
-  protected static _containerGenerator(tag:string) {
+        } else {
+          let vals = attrs[attr] as RawValue[] | RawEAV[];
+           // We have a nested sub-object (i.e. a set of EAVs).
+          if(vals[0].constructor === Array) {
+            let childEavs:RawEAV[] = vals as any;
+            let [childId] = childEavs[0];
+            eavs.push([id, attr, childId]);
+            for(let childEav of childEavs) {
+              eavs.push(childEav);
+            }
+
+          } else {
+            for(let val of vals as RawValue[]) {
+              eavs.push([id, attr, val]);
+            }
+          }
+        }
+      }
+    }
+    return eavs;
+  }
+
+  protected static $elem(tag:string, attrs?: Attrs) {
+    let id = uuid();
+    let eavs:RawEAV[] = [
+      [id, "tag", tag],
+    ];
+    UIWatcher._addAttrs(id, attrs, eavs);
+    return eavs;
+  }
+
+  protected static _makeContainer(tag:string) {
     function $container(children: RawEAV[][]):RawEAV[];
     function $container(attrs: Attrs, children: RawEAV[][]):RawEAV[];
     function $container(attrsOrChildren?: Attrs|RawEAV[][], maybeChildren?: RawEAV[][]):RawEAV[] {
@@ -17,47 +53,38 @@ export class UIWatcher extends Watcher {
       } else {
         children = attrsOrChildren as RawEAV[][];
       }
-      let id = uuid();
-      let EAVs:RawEAV[] = [
-        [id, "tag", tag]
-      ];
-      if(attrs) {
-        for(let attr in attrs) {
-          EAVs.push([id, attr, attrs[attr]]);
-        }
-      }
+
+      let eavs = UIWatcher.$elem(tag, attrs);
+      let [id] = eavs[0];
       for(let child of children) {
         let [childId] = child[0];
-        EAVs.push([id, "children", childId]);
+        eavs.push([id, "children", childId]);
         for(let childEAV of child) {
-          EAVs.push(childEAV);
+          eavs.push(childEAV);
         }
       }
-
-      return EAVs;
+      return eavs;
     }
-
     return $container;
-
   }
 
   public static helpers = {
-    $text: (text:RawValue, attrs?: Attrs) => {
-      let id = uuid();
-      let EAVs:RawEAV[] = [
-        [id, "tag", "ui/text"],
-        [id, "text", text]
-      ];
-      if(attrs) {
-        for(let attr in attrs) {
-          EAVs.push([id, attr, attrs[attr]]);
-        }
-      }
-      return EAVs;
+    $style: (attrs?: Attrs) => {
+      return UIWatcher._addAttrs(uuid(), attrs);
     },
+    $elem: UIWatcher.$elem,
 
-    $row: UIWatcher._containerGenerator("ui/row"),
-    $column: UIWatcher._containerGenerator("ui/column"),
+    $text: (text:RawValue, attrs?: Attrs) => {
+      let eavs = UIWatcher.$elem("ui/text", attrs);
+      let [id] = eavs[0];
+      eavs.push([id, "text", text]);
+      return eavs;
+    },
+    $button: (attrs?: Attrs) => {
+      return UIWatcher.$elem("ui/button", attrs);
+    },
+    $row: UIWatcher._makeContainer("ui/row"),
+    $column: UIWatcher._makeContainer("ui/column"),
   }
 
   public helpers = UIWatcher.helpers;
@@ -66,17 +93,30 @@ export class UIWatcher extends Watcher {
     this.program.attach("html");
 
     this.program
+    // Containers
       .block("Decorate row elements as html.", ({find, record}) => {
-        let elem = find("ui/row", {});
+        let elem = find("ui/row");
         return [elem.add("tag", "html/element").add("tagname", "row")];
       })
       .block("Decorate column elements as html.", ({find, record}) => {
-        let elem = find("ui/column", {});
+        let elem = find("ui/column");
         return [elem.add("tag", "html/element").add("tagname", "column")];
       })
+
+    // Text
       .block("Decorate text elements as html.", ({find, record}) => {
-        let elem = find("ui/text", {});
+        let elem = find("ui/text");
         return [elem.add("tag", "html/element").add("tagname", "text")];
+      })
+
+    // Buttons
+      .block("Decorate button elements as html.", ({find, record}) => {
+        let elem = find("ui/button");
+        return [elem.add("tag", "html/element").add("tagname", "div").add("class", "button")];
+      })
+      .block("Decorate button elements with icons.", ({find, record}) => {
+        let elem = find("ui/button");
+        return [elem.add("class", "iconic").add("class", `ion-${elem.icon}`)];
       })
 
   }
