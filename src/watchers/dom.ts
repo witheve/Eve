b365ef5837db0644ed9454d3dc97a7ddee748ec1
Joshua Cole <joshuafcole@gmail.com>
2017-03-18 20:12:11 -0700

    The dom watcher doesnt track AVs with fine enough granularity to enforce this check

--- src/watchers/dom.ts
+++ src/watchers/dom.ts
@@ -86,7 +86,7 @@ export abstract class DOMWatcher<Instance extends ElemInstance> extends Watcher 
     let style = this.getStyle(styleId);
     if(count === -1) {
       //if(!style[attribute]) throw new Error(`Cannot remove non-existent attribute '${attribute}'`);
-      if(style[attribute] !== value) throw new Error(`Cannot remove mismatched AV ${attribute}: ${value} (current: ${style[attribute]})`);
+      //if(style[attribute] !== value) throw new Error(`Cannot remove mismatched AV ${attribute}: ${value} (current: ${style[attribute]})`);
       style[attribute] = undefined;
     } else {
       if(style[attribute]) throw new Error(`Cannot add already present attribute '${attribute}'`);
