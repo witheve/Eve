bf23a367d53c6471892dd207fc5b3836b753cc75
Joshua Cole <joshuafcole@gmail.com>
2017-05-16 17:45:01 -0400

    Add some more html-specific aliases to ui

--- src/watchers/ui.ts
+++ src/watchers/ui.ts
@@ -137,6 +137,18 @@ export class UIWatcher extends Watcher {
       .bind("Decorate img elements as html.", ({find, record}) => {
         let elem = find("ui/img");
         return [elem.add("tag", "html/element").add("tagname", "img")];
+      })
+      .bind("Decorate h1 elements as html.", ({find, record}) => {
+        let elem = find("ui/h1");
+        return [elem.add("tag", "html/element").add("tagname", "h1")];
+      })
+      .bind("Decorate h2 elements as html.", ({find, record}) => {
+        let elem = find("ui/h2");
+        return [elem.add("tag", "html/element").add("tagname", "h2")];
+      })
+      .bind("Decorate h3 elements as html.", ({find, record}) => {
+        let elem = find("ui/h3");
+        return [elem.add("tag", "html/element").add("tagname", "h3")];
       });
 
 
