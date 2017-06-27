40ef5f5de096488beb1c98b8eadc3c3296a6f32c
cmontella <cmontella@live.com>
2017-06-27 15:07:20 -0700

    change console name

--- src/watchers/console.ts
+++ src/watchers/console.ts
@@ -2,52 +2,52 @@ import * as fs from "fs";
 import {Watcher} from "./watcher";
 import {ID} from "../runtime/runtime";
 
-class FileWatcher extends Watcher {
+class consoleWatcher extends Watcher {
 
     setup() {
-        let {program:me} = this;
+        if(console) {
+            let {program:me} = this;
 
-        me.watch("print to console log", ({find, record}) => {
-            let log = find("console/log");
-            return [
-                record({log, text: log.text})
-            ]
-        })
-        me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
-            Object.keys(adds).forEach((id) => {
-                let {log, text} = adds[id];
-                console.log(text);
+            me.watch("print to console log", ({find, record}) => {
+                let log = find("console/log");
+                return [
+                    record({log, text: log.text})
+                ]
             })
-        })
-
-        me.watch("print to console error", ({find, record}) => {
-            let log = find("console/error");
-            return [
-                record({log, text: log.text})
-            ]
-        })
-        me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
-            Object.keys(adds).forEach((id) => {
-                let {log, text} = adds[id];
-                console.error(text);
+            me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
+                Object.keys(adds).forEach((id) => {
+                    let {log, text} = adds[id];
+                    console.log(text);
+                })
             })
-        })
 
-        me.watch("print to console warn", ({find, record}) => {
-            let log = find("console/warn");
-            return [
-                record({log, text: log.text})
-            ]
-        })
-        me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
-            Object.keys(adds).forEach((id) => {
-                let {log, text} = adds[id];
-                console.warn(text);
+            me.watch("print to console error", ({find, record}) => {
+                let log = find("console/error");
+                return [
+                    record({log, text: log.text})
+                ]
             })
-        })
+            me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
+                Object.keys(adds).forEach((id) => {
+                    let {log, text} = adds[id];
+                    console.error(text);
+                })
+            })
 
-
+            me.watch("print to console warn", ({find, record}) => {
+                let log = find("console/warn");
+                return [
+                    record({log, text: log.text})
+                ]
+            })
+            me.asObjects<{log:ID, text:string}>(({adds, removes}) => {
+                Object.keys(adds).forEach((id) => {
+                    let {log, text} = adds[id];
+                    console.warn(text);
+                })
+            })
+        }
     }
 }
 
-Watcher.register("console", FileWatcher);
+Watcher.register("console", consoleWatcher);
