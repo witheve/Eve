4a4cecd385676621f2cd8bcf4f3148c13917988e
cmontella <cmontella@live.com>
2017-06-27 15:07:38 -0700

    add file write watcher, report errors in Eve

--- src/watchers/file.ts
+++ src/watchers/file.ts
@@ -1,36 +1,82 @@
 import * as fs from "fs";
-import {Watcher} from "./watcher";
+import {Watcher, RawEAVC} from "./watcher";
 import {ID} from "../runtime/runtime";
 
 class FileWatcher extends Watcher {
 
     setup() {
         let {program:me} = this;
+        me.load(`
+Attach errors to the associated file
+~~~
+search
+  error = [#file/error file code message]
 
-        me.watch("read a file", ({find, record}) => {
-            let readfile = find("file/read");
-            return [
-                record({readfile, path: readfile.path})
-            ]
-        })
-
-        me.asObjects<{readfile:ID, path:string}>(({adds, removes}) => {
-            Object.keys(adds).forEach((id) => {
-                let {readfile, path} = adds[id];
-                fs.readFile(path, {encoding: 'utf-8'}, function(err,data){
-                    if (!err) {
-                        let changes:any[] = [];
-                        changes.push(
-                            [readfile, "contents", data, 1],
-                        );
-                        me.inputEAVs(changes);
-                    } else {
-                        console.log(err);
-                    }
-                });
+bind
+  file.error += error
+~~~     
+        `)
+        if(fs.readFile) {
+            me.watch("read a file", ({find, record}) => {
+                let file = find("file/read");
+                return [
+                    record({file, path: file.path, encoding: file.encoding})
+                ]
             })
-        })
 
+            me.asObjects<{file:ID, path:string, encoding:string}>(({adds, removes}) => {
+                Object.keys(adds).forEach((id) => {
+                    let {file, path, encoding} = adds[id];
+                    fs.readFile(path, {encoding: encoding}, function(err, contents){
+                        if (!err) {
+                            let changes:RawEAVC[] = [];
+                            changes.push(
+                                [file, "contents", contents, 1],
+                            );
+                            me.inputEAVs(changes);
+                        } else {
+                            let id = `${file}|error`
+                            let changes:RawEAVC[] = [];
+                            changes.push(
+                                [id, "tag", "file/error", 1],
+                                [id, "file", file, 1],
+                                [id, "code", `${err.code}`, 1],
+                                [id, "message", `${err.message}`, 1]
+                            );
+                            me.inputEAVs(changes);
+                        }
+                    });
+                })
+            })
+        }
+        if(fs.writeFile) {
+            me.watch("write a file", ({find, record}) => {
+                let file = find("file/write");
+                return [
+                    record({file, path: file.path, encoding: file.encoding, contents: file.contents})
+                ]
+            })
+            me.asObjects<{file:ID, path:string, contents: string, encoding:string}>(({adds, removes}) => {
+                Object.keys(adds).forEach((id) => {
+                    let {file, path, contents, encoding} = adds[id];
+                    fs.writeFile(path, contents, {encoding: encoding}, function(err){
+                        if (!err) {
+                            console.log(`Write file success: ${contents}`)
+                        } else {
+                            let id = `${file}|error`
+                            let changes:RawEAVC[] = [];
+                            changes.push(
+                                [id, "tag", "file/error", 1],
+                                [id, "file", file, 1],
+                                [id, "code", `${err.code}`, 1],
+                                [id, "message", `${err.message}`, 1]
+                            );
+                            me.inputEAVs(changes);
+                        }
+                    });
+                })
+            })
+        }
     }
 }
 
