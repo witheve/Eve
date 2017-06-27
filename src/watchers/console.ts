03f8644e01a4a9290c8c9dbee06fac02452e7da6
cmontella <cmontella@live.com>
2017-06-27 15:47:37 -0700

    fix tabs

--- src/watchers/console.ts
+++ src/watchers/console.ts
@@ -4,44 +4,38 @@ import {ID} from "../runtime/runtime";
 
 class ConsoleWatcher extends Watcher {
 
-    setup() {
-        if(console) {
-            this.program
-            .watch("Print to console log.", ({find, record}) => {
-                let log = find("console/log");
-                return [
-                    record({log, text: log.text})
-                ]
-            })
-            .asDiffs(({adds}) => {
-                for(let [log, _, text] of adds) {
-                    console.log(text);
-                }
-            })
-            .watch("Print to console error.", ({find, record}) => {
-                let log = find("console/error");
-                return [
-                    record({log, text: log.text})
-                ]
-            })
-            .asDiffs(({adds}) => {
-                for(let [log, _, text] of adds) {
-                    console.error(text);
-                }
-            })
-            .watch("Print to console warn.", ({find, record}) => {
-                let log = find("console/warn");
-                return [
-                    record({log, text: log.text})
-                ]
-            })
-            .asDiffs(({adds}) => {
-                for(let [log, _, text] of adds) {
-                    console.warn(text);
-                }
-            })
+  setup() {
+    if(console) {
+      this.program
+      .watch("Print to console log.", ({find, record}) => {
+        let log = find("console/log");
+          return [record({log, text: log.text})]
+      })
+      .asDiffs(({adds}) => {
+        for(let [log, _, text] of adds) {
+          console.log(text);
         }
+      })
+      .watch("Print to console error.", ({find, record}) => {
+        let log = find("console/error");
+        return [record({log, text: log.text})]
+      })
+      .asDiffs(({adds}) => {
+        for(let [log, _, text] of adds) {
+          console.error(text);
+        }
+      })
+      .watch("Print to console warn.", ({find, record}) => {
+        let log = find("console/warn");
+        return [record({log, text: log.text})]
+      })
+      .asDiffs(({adds}) => {
+        for(let [log, _, text] of adds) {
+          console.warn(text);
+        }
+      })
     }
+  }
 }
 
 Watcher.register("console", ConsoleWatcher);
